ab9025e031585b6c25df718fa4b007d7a75f7db6
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-28 00:13:14 +0100

    Cleaned up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import styled from 'styled-components'
 
 import GlobalStyles from './GlobalStyles'
 
+import StartPage from './components/StartPage'
+import Login from './components/Login'
+import Register from './components/Register'
+import Home from './components/Home'
 import Articles from './components/Articles'
 import Article from './components/Article'
-import Login from './components/Login'
 import ProtectedRoute from './components/ProtectedRoute'
 
-interface AppProps {
-  title: string
-  initialCount?: number
-}
-
+// can be used with styled.div<Interface|Types|Props>
 const StyledContainer = styled.div`
   background-color: #f0f0f0;
   padding: 20px;
 `
 
+// can be used with styled.h1<Interface|Types|Props>
 const StyledTitle = styled.h1`
   color: blue;
 `
 
-const App: FC<AppProps> = ({ title, initialCount = 0 }) => {
-  const [count, setCount] = useState<number>(initialCount)
-
+const App: FC = () => {
   return (
     <Router>
       <GlobalStyles />
       <Routes>
+        <Route path="/" element={<StartPage />} />
         <Route path="/login" element={<Login />} />
-        <Route element={<ProtectedRoute />}> {/* Routes that require authentication */}
-          <Route path="/" element={<Articles />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route element={<ProtectedRoute />}> */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/articles" element={<Articles />} />
           <Route path="/articles/:id" element={<Article />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} /> {/* Redirects to / if route not found */}
+        {/* </Route> */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   )
