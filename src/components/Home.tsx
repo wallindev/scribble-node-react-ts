b3b5c7d2c961cea5a6e3041702ab25ba607a3c18
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-29 00:31:22 +0100

    Added Home component/page as landing page for logged in users

---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+
+const StartPage: FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Your Home Page!</h1>
+      <p>
+        Here you can manage your <Link to="/articles">Articles</Link> and your <Link to="/profile">Profile</Link>.<br />
+        Scribble away, and save all your thoughts and ideas in one place!
+      </p>
+      <div> {/* Container for the links */}
+        <Link to="/articles" style={{ marginRight: '20px' }}>Articles</Link>
+        <Link to="/profile">Profile</Link>
+      </div>
+    </div>
+  );
+}
+
+export default StartPage
