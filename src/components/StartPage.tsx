1ae49277eecaff897dc00bd0ed92787ae72947f3
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-29 00:25:20 +0100

    Removed local, extraneous styling and cleaned up text.

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,20 +1,12 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
 
-import GlobalStyle from './components/GlobalStyles'
-
 const StartPage: FC = () => {
   return (
-    <div style={{ 
-      display: 'flex', 
-      flexDirection: 'column', 
-      alignItems: 'center', 
-      justifyContent: 'center', 
-      height: '100vh' // Make it take up the full viewport height
-    }}>
-      <h1>Welcome to the Articles App</h1>
-      <p style={{ textAlign: 'center', maxWidth: '600px', margin: '20px 0' }}>
-        This is a platform where you can discover, create, and manage your articles.  
+    <div>
+      <h1>Welcome to Scribble!</h1>
+      <p>
+        Scribble! - a tiny web app where you can create and manage your articles.<br />
         Join our community and share your thoughts with the world!
       </p>
       <div> {/* Container for the links */}
