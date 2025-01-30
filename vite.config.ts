dfc46620685d74aeb43fd0dd3125f3629f2972f5
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-30 22:39:33 +0100

    Changed from setting host and port programmatically (hardcoded) in package.json to declaratively in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'grungecorp.dev', // 0.0.0.0 for all interfaces
+    port: 8000,
+  },
 })
