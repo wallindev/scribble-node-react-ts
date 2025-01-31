6e0a43024a6881308bfc3356df5294a9000c9a4b
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-31 22:48:56 +0100

    Removed line-height so that all text has uniform height regardless of type of button

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -10,7 +10,7 @@ const GlobalStyle = createGlobalStyle`
     --font-size-base: 16px;
 
     font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-    line-height: 1.5;
+    /* line-height: 1.5; */
     font-weight: 400;
 
     color-scheme: dark light;
