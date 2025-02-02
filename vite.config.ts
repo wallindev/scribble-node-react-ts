c68c31e9ec49d933ca00076f8256d8b387246888
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-02 02:58:07 +0100

    Upgraded to SWC (from Babel?), and set Vite frontend port to 5002 (to be able to run the Laravel app simultaneously with Vite on port 5001)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: 'grungecorp.dev', // 0.0.0.0 for all interfaces
-    port: 8000,
+    port: 5002,
   },
 })
