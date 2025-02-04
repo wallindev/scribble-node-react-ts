04f3bafeed1626286fc51770ab9eb15c57937ecc
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-04 18:34:18 +0100

    Removed unused styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import styled from 'styled-components'
 
-import GlobalStyles from './GlobalStyles'
+import GlobalStyle from './GlobalStyle'
 
 import StartPage from './components/StartPage'
 import Login from './components/Login'
@@ -12,31 +11,19 @@ import Articles from './components/Articles'
 import Article from './components/Article'
 import ProtectedRoute from './components/ProtectedRoute'
 
-// can be used with styled.div<Interface|Types|Props>
-const StyledContainer = styled.div`
-  background-color: #f0f0f0;
-  padding: 20px;
-`
-
-// can be used with styled.h1<Interface|Types|Props>
-const StyledTitle = styled.h1`
-  color: blue;
-`
-
 const App: FC = () => {
   return (
     <Router>
-      <GlobalStyles />
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route element={<ProtectedRoute />}> */}
+        <Route element={<ProtectedRoute />}>
           <Route path="/home" element={<Home />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="/articles/:id" element={<Article />} />
-          <Route path="/articles/new" element={<Article />} />
-        {/* </Route> */}
+        </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
