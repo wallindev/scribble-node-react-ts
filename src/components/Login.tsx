2d85f6ac388cfa0177e4f44ecb7ce9ddd3eee408
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-05 19:28:32 +0100

    Rearranged some code

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,30 +1,48 @@
-import { FC, useState } from 'react'
-import { Navigate } from 'react-router-dom'
-import axios from 'axios'
+import { FC, FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+// import axios from 'axios'
+
+import AuthForm from './shared/AuthForm'
 
 const Login: FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const navigate = useNavigate()
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    location.href='/home'
-    // try {
-    //   const response = await axios.post('/api/login', { username, password }); // Your Laravel login route
-    //   localStorage.setItem('token', response.data.token); // Store the token
-    //   window.location.href = '/'; // Redirect to the article list
-    // } catch (error) {
-    //   console.error('Login error:', error);
-    //   // Handle error (e.g., display a message)
-    // }
-  };
+  const handleLogin = (username: string, password: string) => {
+    if (username === 'testuser' && password === 'password') {
+      localStorage.setItem('AUTHENTICATED', 'true')
+      navigate('/home')
+    } else {
+      alert('Invalid credentials')
+    }
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* Input fields for username and password */}
-      <button type="submit">Login</button>
-    </form>
-  );
-};
+    <AuthForm formType="Login" onSubmit={handleLogin} />
+  )
+}
+
+// const Login: FC = () => {
+//   const [username, setUsername] = useState('')
+//   const [password, setPassword] = useState('')
+
+//   const handleSubmit = async (e: FormEvent) => {
+//     e.preventDefault()
+//     try {
+//       const response = await axios.post('/api/login', { username, password }) // Your Laravel login route
+//       localStorage.setItem('token', response.data.token) // Store the token
+//       navigate('/home') // Redirect to the user home page
+//     } catch (error) {
+//       console.error('Login error:', error)
+//       // Handle error (e.g., display a message)
+//     }
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       {/* Input fields for username and password */}
+//       <button type="submit">Login</button>
+//     </form>
+//   )
+// }
 
 export default Login
