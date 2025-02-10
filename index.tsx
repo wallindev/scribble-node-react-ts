ce40c3b7dea9bcfa5600cf0913a7e22198e1fdc1
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-10 22:10:32 +0100

    Removed file extension in import and extraneous comma

--- index.tsx
+++ index.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import App from './src/App.tsx'
+import App from './src/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
