1cc7a6fa650344825d9788cf142a05cd9e32f4c0
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-11 22:31:45 +0100

    Cleaned up, optimized, and sorted the code, moved handling of loading state to App and loading component to Layout, added deleteArticle function with error handling in Axios requests

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,52 +1,100 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import type { FC, JSX } from 'react'
 import type { AxiosResponse } from 'axios'
 import Layout from './layout/Layout'
 import CustomButton from './shared/CustomButton'
+import FlashMessage from './shared/FlashMessage'
 import TextLink from './shared/TextLink'
-import LoadText from './shared/LoadText'
-import type { IArticle } from '../types/article.types'
+import type { TArticle, IGlobal, TFlashMessage } from '../types/general.types'
+import { dismissFlashMessage } from '../utils/functions'
+import { defaultFlashMessage } from '../utils/defaults'
 
-const Articles: FC = (): JSX.Element => {
+const Articles: FC<IGlobal> = ({ loading, setLoading, theme, setTheme }): JSX.Element => {
   const navigate = useNavigate()
-  const [articles, setArticles] = useState<IArticle[]>([])
-  const [loading, setLoading] = useState(true)
-  // const apiEndPoint = '/api/articles'
-  const apiEndPointMock = 'http://192.168.32.2:8000/articles'
+  const [articles, setArticles] = useState<TArticle[]>([])
+  const [flashMessage, setFlashMessage] = useState<TFlashMessage>(defaultFlashMessage)
+
+  const deleteArticle = (artcl: TArticle): void => {
+    if (confirm(`Do you really want to delete article '${artcl.title}'`)) {
+      !(async (): Promise<void> => {
+        setLoading!(true)
+        try {
+          const response: AxiosResponse = await axios.delete(`${'/articles'}/${artcl.id}`/* , {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+          } */)
+          // console.log('response.data:', response.data)
+          const deletedArticle = response.data
+          const newArticles = articles.filter(article => article.id !== deletedArticle.id)
+          setArticles(newArticles)
+        } catch (error) {
+          console.error('Error deleting article:', error)
+          setFlashMessage({
+            message: `Error updating article:<br />${error}`,
+            type: 'error',
+            visible: true,
+          })
+        } finally {
+          setFlashMessage({
+            message: 'Article removed successfully',
+            type: 'success',
+            visible: true,
+          })
+          console.log('Deleted article', artcl.id)
+          // To mock slow network
+          // setTimeout(() => {
+            setLoading!(false)
+          // }, 5000)
+        }
+      })()
+    }
+  }
 
   useEffect(() => {
-    /* const fetchArticles =  */(async (): Promise<void> => {
-      // setLoading(true)
+    !(async (): Promise<void> => {
+      setLoading!(true)
       try {
-        const response: AxiosResponse = await axios.get(apiEndPointMock/* , {
+        const response: AxiosResponse = await axios.get('/articles'/* , {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
         } */)
+        console.log(response.data)
         setArticles(response.data)
       } catch (error) {
         console.error('Error fetching articles:', error)
       } finally {
-        setLoading(false)
+        // To mock slow network
+        // setTimeout(() => {
+          setLoading!(false)
+        // }, 5000)
       }
     })()
-
-    // Simulate throttling (to be able to see the LoadText component)
-    // setTimeout(() => {
-    //   fetchArticles()
-    // }, 5000)
   }, [])
 
-  return/*  loading ? <LoadText loading={loading} /> : */ (
-    <Layout>
-      <div className="m-2">
+  return (
+    <Layout loading={loading} theme={theme} setTheme={setTheme}>
+      {flashMessage.visible && (
+        <FlashMessage
+          message={flashMessage.message}
+          type={flashMessage.type}
+          onDismiss={() => dismissFlashMessage(flashMessage, setFlashMessage)}
+        />
+      )}
+      <div className="mt-0 m-2 mx-auto">
         <CustomButton onClick={() => navigate('/articles/new')}>New Article &raquo;</CustomButton>
-        <div className="my-2">
+        <div className="m-2">
           {articles.map((article) => (
-            <div key={article.id} className="my-1">
+            <div className="my-2 flex flex-row justify-between items-center border-0 border-b-1" key={article.id}>
+              <div key={`t${article.id}`} className="my-1">
                 <TextLink to={`/articles/${article.id}`} className="text-lg">{article.title}</TextLink>
+              </div>
+              <div key={`d${article.id}`} className="my-1">
+                <CustomButton onClick={() => deleteArticle(article)}>Delete</CustomButton>
+              </div>
             </div>
           ))}
         </div>
