1e70f0df25f388beb8cd37a8d8cbaf86a780a793
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-12 22:45:36 +0100

    Updated handleRegister function, added Layout, and removed old code

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,45 +1,33 @@
-import { FC, FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 // import axios from 'axios'
+import type { FC, JSX } from 'react'
+import Layout from './layout/Layout'
+import AuthForm from './shared/AuthForm'
+import type { IGlobal } from '../types/general.types'
+import { setAuth } from '../utils/functions'
 
-import AuthForm from './shared/AuthForm';
+const Register: FC<IGlobal> = ({ loading, theme, setTheme }): JSX.Element => {
+  const navigate = useNavigate()
 
-const Register: FC = () => {
-  const navigate = useNavigate();
-
-  const handleRegister = (username: string, password: string) => {
-    console.log("Registering user:", username, password); // Log the values (or make a real API call)
-    localStorage.setItem('isAuthenticated', 'true');
-    navigate('/home');
-  };
+  const handleRegister = (username: string, password: string): void => {
+    // try {
+    //   const response = await axios.post('/api/login', { username, password }) // Your Laravel login route
+    //   localStorage.setItem('token', response.data.token) // Store the token
+    //   '/' // Redirect to the article list
+    // } catch (error) {
+    //   console.error('Register error:', error)
+    //   // Handle error (e.g., display a message)
+    // }
+    console.log("Registering user:", username, password) // Log the values (or make a real API call)
+    setAuth(true)
+    navigate('/home')
+  }
 
   return (
-    <AuthForm formType="Register" onSubmit={handleRegister} />
-  );
-};
-
-// const Register: FC = () => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   const handleSubmit = async (e: FormEvent) => {
-//     e.preventDefault();
-//     try {
-//       const response = await axios.post('/api/login', { username, password }); // Your Laravel login route
-//       localStorage.setItem('token', response.data.token); // Store the token
-//       window.location.href = '/'; // Redirect to the article list
-//     } catch (error) {
-//       console.error('Register error:', error);
-//       // Handle error (e.g., display a message)
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       {/* Input fields for username and password */}
-//       <button type="submit">Register</button>
-//     </form>
-//   );
-// };
+    <Layout loading={loading} theme={theme} setTheme={setTheme}>
+      <AuthForm formType="register" onSubmit={handleRegister} />
+    </Layout>
+  )
+}
 
 export default Register
