b3a79dc5c415b6f066667324a628d48ac5ee6ac6
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-12 22:41:32 +0100

    Added Layout, updated handleLogin function, and removed old code

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,26 @@
-import { FC, FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 // import axios from 'axios'
-
+import type { FC, JSX } from 'react'
+import Layout from './layout/Layout'
 import AuthForm from './shared/AuthForm'
+import type { IGlobal } from '../types/general.types'
+import { setAuth } from '../utils/functions'
 
-const Login: FC = () => {
+const Login: FC<IGlobal> = ({ loading, theme, setTheme }): JSX.Element => {
   const navigate = useNavigate()
 
-  const handleLogin = (username: string, password: string) => {
-    if (username === 'testuser' && password === 'password') {
-      localStorage.setItem('AUTHENTICATED', 'true')
+  const handleLogin = async (username: string, password: string): Promise<void> => {
+    // try {
+    //   const response: AxiosResponse = await axios.post('/api/login', { username, password }) // Your Laravel login route
+    //   localStorage.setItem('token', response.data.token) // Store the token
+    //   navigate('/home') // Redirect to the user home page
+    // } catch (error) {
+    //   console.error('Login error:', error)
+    //   // Handle error (e.g., display a message)
+    // }
+    if (username === 'mikael' && password === 'grunge' ||
+      username === 'lisbeth' && password === 'larsson') {
+      setAuth(true)
       navigate('/home')
     } else {
       alert('Invalid credentials')
@@ -17,32 +28,10 @@ const Login: FC = () => {
   }
 
   return (
-    <AuthForm formType="Login" onSubmit={handleLogin} />
+    <Layout loading={loading} theme={theme} setTheme={setTheme}>
+      <AuthForm formType="login" onSubmit={handleLogin} />
+    </Layout>
   )
 }
-
-// const Login: FC = () => {
-//   const [username, setUsername] = useState('')
-//   const [password, setPassword] = useState('')
-
-//   const handleSubmit = async (e: FormEvent) => {
-//     e.preventDefault()
-//     try {
-//       const response = await axios.post('/api/login', { username, password }) // Your Laravel login route
-//       localStorage.setItem('token', response.data.token) // Store the token
-//       navigate('/home') // Redirect to the user home page
-//     } catch (error) {
-//       console.error('Login error:', error)
-//       // Handle error (e.g., display a message)
-//     }
-//   }
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       {/* Input fields for username and password */}
-//       <button type="submit">Login</button>
-//     </form>
-//   )
-// }
 
 export default Login
