38cf622496c814913a7500a01a98494b0d5e7011
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-13 23:13:09 +0100

    Completely new, revised auth form with the new AuthForm props and Interface

--- src/components/shared/AuthForm.tsx
+++ src/components/shared/AuthForm.tsx
@@ -1,33 +1,69 @@
-import { FC, useState } from 'react'
-import { AuthFormProps } from '../../types/form.types';
+import { useState } from 'react'
+import type { ChangeEvent, FC, FormEvent } from 'react'
+import TextInput from './TextInput'
+import CustomButton from './CustomButton'
+import type { IAuthForm } from '../../types/form.types'
 
-const AuthForm: FC<AuthFormProps> = ({ formType, onSubmit }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleSubmit = (e: React.FormEvent) => {
-      e.preventDefault();
-      onSubmit(username, password);
-    };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>{formType}</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">{formType}</button>
-      </form>
-    );
+const AuthForm: FC<IAuthForm> = ({ formType, onSubmit }) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault()
+    onSubmit(username, password)
   }
 
-  export default AuthForm
+  const headline = `${formType.charAt(0).toUpperCase()}${formType.slice(1)}`
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="text-2xl mb-4">{headline}</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {formType === 'register' &&
+        <>
+          <div>
+            <label htmlFor="firstName">First name:</label>
+            <TextInput
+              id="firstName"
+              type="text"
+              value={firstName}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last name:</label>
+            <TextInput
+              id="lastName"
+              type="text"
+              value={lastName}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
+            />
+          </div>
+        </>}
+        <div>
+          <label htmlFor="username">Username:</label>
+          <TextInput
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <TextInput
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+      <CustomButton type="submit">{headline}</CustomButton>
+    </form>
+  )
+}
+
+export default AuthForm
