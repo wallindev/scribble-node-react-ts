77d7d4a2fe1f01ad4703c6d576c49b4e4a4aae31
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-13 23:18:39 +0100

    Removed styled components styling and moved out the interface to the shared type file

--- src/components/shared/FormButton.tsx
+++ src/components/shared/FormButton.tsx
@@ -1,42 +1,11 @@
-import { FC } from 'react'
-import styled from 'styled-components'
+import type { FC } from 'react'
+import type { IFormButton } from '../../types/form.types'
 
-// Define the styled component with TypeScript
-const StyledFormButton = styled.button`
-  display: inline-block;
-  padding: 10px 20px;
-  background-color: #007bff;
-  color: white;
-  text-decoration: none;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-
-  &:visited {
-    color: white;
-  }
-
-  &:hover {
-    background-color: #0056b3;
-    color: white;
-  }
-`
-
-// Define the props interface for the FormButton component
-interface FormButtonProps {
-  onClick?: React.MouseEventHandler<HTMLButtonElement> // Correct type for onClick
-  children?: React.ReactNode
-  style?: React.CSSProperties
-  type?: 'button' | 'submit' | 'reset'; // Add type prop for button functionality
-  [key: string]: any // Index signature for other HTML attributes
-}
-
-// Create the FormButton component with TypeScript
-const FormButton: FC<FormButtonProps> = ({ onClick, children, style, type = 'button', ...props }) => {
+const FormButton: FC<IFormButton> = ({ className, onClick, children, type = 'button', ...props }) => {
   return (
-    <StyledFormButton onClick={onClick} style={style} type={type} {...props}>
+    <button className={className} onClick={onClick} type={type} {...props}>
       {children}
-    </StyledFormButton>
+    </button>
   )
 }
 
