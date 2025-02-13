0d948a431144e5d0f5641ec0317f4c0e16a939ff
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-13 23:22:46 +0100

    Imported types and created interfaces/component props

--- src/types/form.types.ts
+++ src/types/form.types.ts
@@ -1,7 +1,42 @@
-export interface AuthFormProps {
-  formType: 'Login' | 'Register'
+import type { MouseEventHandler, ChangeEventHandler, ReactNode } from 'react'
+
+export interface IAuthForm {
+  formType: 'login' | 'register'
   onSubmit: (username: string, password: string) => void
 }
 
-// Type for authentication status (adjust as needed)
-export type AuthStatus = boolean
+export interface ICustomButton {
+  // button: boolean
+  className?: string
+  type?: 'button' | 'submit' | 'reset'
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  size?: 'small' | 'large'
+  to?: string
+  children: ReactNode
+  [key: string]: any
+}
+
+export interface ITextInput {
+  className?: string
+  id?: string
+  name?: string
+  type: string
+  value?: string
+  onChange?: ChangeEventHandler<HTMLInputElement>
+  [key: string]: any
+}
+
+export interface IFormButton {
+  className?: string
+  children?: ReactNode
+  type?: 'button' | 'submit' | 'reset'
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  [key: string]: any
+}
+
+export interface ILinkButton {
+  className?: string
+  to: string
+  children?: ReactNode
+  [key: string]: any
+}
