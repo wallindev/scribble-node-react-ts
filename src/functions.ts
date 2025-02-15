9a96349d862252b9681b8b82db3aff907ede9d13
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-15 23:36:21 +0100

    Moved functions file to util directory

--- src/functions.ts
+++
@@ -1,9 +0,0 @@
-
-import { AuthStatus } from "./types/form.types";
-
-// Helper function to check authentication status
-export const isAuthenticated = (): AuthStatus => {
-  // TODO: Change this to localStorage.getItem('token')
-  // when full authentication is implemented
-  return localStorage.getItem('AUTHENTICATED') === 'true';
-};
