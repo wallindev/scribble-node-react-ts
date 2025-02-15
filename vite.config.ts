0f4bff46ecace3003c61bd92759f4fe777ce511f
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-15 23:46:58 +0100

    Change Vite config file to have a custom config that loads both app- and node-level env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import injectEnvToHtml from './plugins/inject-env-to-html.ts'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    host: 'grungecorp.dev', // 0.0.0.0 for all interfaces
-    port: 5002,
-  },
-})
+/** @type {import('vite').UserConfig} */
+export default ({ mode = 'development' }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
+  }
+
+  return defineConfig({
+    /* mode: mode || 'development', */
+    plugins: [
+      react(),
+      tailwindcss(),
+      injectEnvToHtml(),
+    ],
+    server: {
+      // hmr: false,
+      // host: 'localhost', // 0.0.0.0 for all interfaces
+      port: 5002,
+      /* allowedHosts: [
+        '127.0.0.1',
+        'localhost',
+      ], */
+      watch: {
+        ignored: ['**/api-data/*.*', './TODO.txt'],
+      },
+    }
+  })
+}
