e0f8cbb8e87fbae833a6368b45b9fa3cb746b84c
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-22 20:24:57 +0100

    Added headline and made the componpent/page nicer looking

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -62,7 +62,7 @@ const Articles: FC<IGlobal> = ({ loading, setLoading, theme, setTheme }): JSX.El
             Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
         } */)
-        console.log(response.data)
+        // console.log(response.data)
         setArticles(response.data)
       } catch (error) {
         console.error('Error fetching articles:', error)
@@ -85,14 +85,15 @@ const Articles: FC<IGlobal> = ({ loading, setLoading, theme, setTheme }): JSX.El
         />
       )}
       <div className="mt-0 m-2 mx-auto">
-        <CustomButton onClick={() => navigate('/articles/new')}>New Article &raquo;</CustomButton>
+        <h1 className="text-2xl font-bold mb-4">Articles</h1>
+        <CustomButton className="my-2" onClick={() => navigate('/articles/new')}>New Article &raquo;</CustomButton>
         <div className="m-2">
           {articles.map((article) => (
             <div className="my-2 flex flex-row justify-between items-center border-0 border-b-1" key={article.id}>
-              <div key={`t${article.id}`} className="my-1">
-                <TextLink to={`/articles/${article.id}`} className="text-lg">{article.title}</TextLink>
+              <div className="my-1" key={`t${article.id}`}>
+                <TextLink className="text-xl" to={`/articles/${article.id}`} style={{ fontSize: '20px' }}>{article.title}</TextLink>
               </div>
-              <div key={`d${article.id}`} className="my-1">
+              <div className="my-1" key={`d${article.id}`}>
                 <CustomButton onClick={() => deleteArticle(article)}>Delete</CustomButton>
               </div>
             </div>
