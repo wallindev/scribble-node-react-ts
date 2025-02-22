a20744d9770cf0655858a6490b4e92a25f5f8ad7
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-22 20:26:49 +0100

    Removed Logout button and made the design of the page conform to the rest of the app

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,16 @@
 import type { FC, JSX } from 'react'
 import Layout from './layout/Layout'
-import Logout from './shared/Logout'
+// import Logout from './shared/Logout'
 import TextLink from './shared/TextLink'
 import type { IGlobal } from '../types/general.types'
-import { isAuthenticated } from '../utils/functions'
 
 const Home: FC<IGlobal> = ({ loading, theme, setTheme }): JSX.Element => {
   return (
     <Layout loading={loading} theme={theme} setTheme={setTheme}>
       <div>
         <h1 className="text-2xl font-bold mb-4">Welcome to Your Home Page!</h1>
-        <p className="mb-1">Here you can manage your <TextLink to="/articles">Articles</TextLink> and your <TextLink to="/profile">Profile</TextLink>.</p>
+        <p className="mb-2">Here you can manage your <TextLink to="/articles">Articles</TextLink> and your <TextLink to="/profile">Profile</TextLink>.</p>
         <p className="mb-1">Scribble away, and save all your thoughts and ideas in one place!</p>
-        {isAuthenticated() && <div className="mt-2"><Logout /></div>}
       </div>
     </Layout>
   )
