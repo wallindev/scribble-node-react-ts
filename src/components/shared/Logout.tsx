8ea594d7740fa4e801e9a20d0ec34b2d8465d5b3
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-23 20:40:43 +0100

    Added new function logout that incorporates auth handling and added props spread to component configuration

--- src/components/shared/Logout.tsx
+++ src/components/shared/Logout.tsx
@@ -1,19 +1,20 @@
 import { useNavigate } from 'react-router-dom'
 import type { FC, JSX, MouseEvent } from 'react'
 import CustomButton from './CustomButton'
-import { setAuth } from '../../utils/functions'
+import { logout } from '../../utils/functions'
+import type { ILogout } from '../../types/form.types'
 
-const Logout: FC = (): JSX.Element => {
+const Logout: FC<ILogout> = ({ size = 'large', ...props }): JSX.Element => {
   const navigate = useNavigate()
 
   const handleLogout = (e: MouseEvent<HTMLButtonElement>): void => {
     e.preventDefault()
-    setAuth(false)
+    logout()
     navigate('/')
   }
 
   return (
-    <CustomButton onClick={handleLogout}>Logout</CustomButton>
+    <CustomButton onClick={handleLogout} size={size} {...props}>Logout</CustomButton>
   )
 }
 
