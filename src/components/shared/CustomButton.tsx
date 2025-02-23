7e00a9cc39b6c8706035c5114570818ea0ae35e8
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-23 20:38:46 +0100

    Organized css for the different sizes of the button, added span around the button text

--- src/components/shared/CustomButton.tsx
+++ src/components/shared/CustomButton.tsx
@@ -4,13 +4,22 @@ import type { ICustomButton } from '../../types/form.types'
 
 const CustomButton: FC<ICustomButton> = ({ onClick, to, type, children, className, size = 'large', ...props }): JSX.Element => {
   let classNamesButton = 'inline-block bg-button-bg text-button-text border-0 rounded-sm [&:hover,&:active,&:focus]:bg-button-bg--hover outline-0 cursor-pointer transition-colors duration-300'
-  classNamesButton = size === 'large' ? `${classNamesButton} py-1 px-4` : `${classNamesButton} p-1 text-xs`
+  switch (size) {
+    case 'small':
+      classNamesButton = `${classNamesButton} leading-[1.2] p-1 px-2 text-xs`
+      break;
+    case 'medium':
+      classNamesButton = `${classNamesButton} leading-[1.2] p-1.5 px-3`
+      break;
+    case 'large':
+      classNamesButton = `${classNamesButton} leading-[1.2] py-2 px-4 text-lg`
+      break;
+    default:
+  }
   let classNamesLink = `${classNamesButton} hover: no-underline [&:visited,&:hover]:text-button-text`
-  // class="pointer-events-none select-none"
-  to ?
-    classNamesLink = className ? `${classNamesLink} ${className}` : classNamesLink
-  :
-    classNamesButton = className ? `${classNamesButton} ${className}` : classNamesButton
+  // class="pointer-events-none select-none text-button-text leading-[1.2]"
+  to ? classNamesLink = className ? `${classNamesLink} ${className}` : classNamesLink :
+       classNamesButton = className ? `${classNamesButton} ${className}` : classNamesButton
 
   return to ? (
     <Link className={classNamesLink} to={to} {...props}>
@@ -18,7 +27,7 @@ const CustomButton: FC<ICustomButton> = ({ onClick, to, type, children, classNam
     </Link>
   ) : (
     <button className={classNamesButton} type={type} onClick={onClick} {...props}>
-      {children}
+      <span className="text-inherit pointer-events-none">{children}</span>
     </button>
   )
 }
