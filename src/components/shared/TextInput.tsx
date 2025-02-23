a6e080cb297f63a0d53aa4bd61950b264fa66e81
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-23 20:46:29 +0100

    Removed CSS that should not be in the component so that the TextInput is easier to customize, added disabled prop

--- src/components/shared/TextInput.tsx
+++ src/components/shared/TextInput.tsx
@@ -1,12 +1,12 @@
 import type { FC, JSX } from 'react'
 import type { ITextInput } from '../../types/form.types'
 
-const TextInput: FC<ITextInput> = ({ className, id, type, value, ...props }): JSX.Element => {
-  let classNames = 'p-1 bg-input-bg text-input-text w-full sm:w-4/5 border-0 outline-0 rounded-sm'
-  className ? `${classNames} ${className}` : className
+const TextInput: FC<ITextInput> = ({ className, id, type, value, disabled, ...props }): JSX.Element => {
+  let classNames = 'border-0 outline-0 rounded-sm'
+  classNames = className ? `${classNames} ${className}` : classNames
 
   return (
-    <input className={classNames} id={id} type={type} value={value} {...props} />
+    <input className={classNames} id={id} type={type} value={value} disabled={disabled} {...props} />
   )
 }
 
