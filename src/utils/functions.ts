1da07d4781c7f63060e653d74c2c5c5a47ade309
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-24 20:57:43 +0100

    Added login and logout function, getters and setters for the user id that is saved in local storage, and added a custom error logging/printing function that prints the error with the same colors as console.error in the browser js console but with more concise information

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,8 @@
 // import { useState } from 'react'
 import type { Dispatch, SetStateAction } from 'react'
 import type { TFlashMessage } from '../types/general.types'
+import { isAxiosError } from 'axios'
+import type { AxiosError } from 'axios'
 
 // Example of JSDoc
 /**
@@ -16,8 +18,9 @@ import type { TFlashMessage } from '../types/general.types'
  */
 export const setElementText = (element: HTMLDivElement, text: string) => element.innerText = text
 
-export const selectElementText = (element: HTMLDivElement, text?: string): void => {
-  if (element && element.innerText === text) {
+export const selectElementText = (element: HTMLDivElement | HTMLInputElement, text?: string): void => {
+  if (((element as HTMLDivElement) && (element as HTMLDivElement).innerText === text) ||
+      ((element as HTMLInputElement) && (element as HTMLInputElement).value === text)) {
     const range = document.createRange()
     range.selectNodeContents(element)
     // const selection = window.getSelection()
@@ -42,14 +45,67 @@ export const dismissFlashMessage = (
 export const isAuthenticated = (): boolean => {
   // TODO: Change this to localStorage.getItem('token')
   // when full authentication is implemented
-  return localStorage.getItem('AUTHENTICATED') === 'true'
+  return localStorage.getItem('AUTHENTICATED') === 'true' && hasUserId()
+}
+
+export const login = (): void => {
+  setAuth(true)
+  setUserId(1)
+}
+
+export const logout = (): void => {
+  setAuth(false)
 }
 
 export const setAuth = (authenticated: boolean): void => {
   authenticated ? localStorage.setItem('AUTHENTICATED', 'true') : localStorage.removeItem('AUTHENTICATED')
 }
 
+export const getUserId = (): number | null => {
+  const userId: string | null = localStorage.getItem('userId')
+  if (userId || Number.isInteger(Number(userId))) {
+    return Number(userId)
+  } else {
+    return null
+  }
+}
+
+export const hasUserId = (): boolean => {
+  return localStorage.getItem('userId') !== null
+}
+
+export const setUserId = (userId: number): void => {
+  localStorage.setItem('userId', userId.toString())
+}
+
+export const removeUserId = (): void => {
+  localStorage.removeItem('userId')
+}
+
 export const localDateStr = (dateStr?: string | null): string => {
   const date = dateStr ? new Date(dateStr) : new Date()
   return date.toLocaleString("sv-SE", { timeZone: "CET" })
+}
+
+// console.log with console.error styling
+export const consoleError = (errorObj: any): void => {
+  const style: string = `display: flex;
+align-items: center;
+padding: 5px 10px;
+background: #4B2F36;
+color: #F4ABC9;
+border-top: 1px solid #743E4C;
+border-bottom: 1px solid #743E4C;`.replace(/\n/g, ' ')
+  if (isAxiosError(errorObj)) {
+    const error: AxiosError = errorObj as AxiosError
+    console.log(`%c${error.name}: ${error.message}`, style)
+  } else {
+    console.log(`%c${errorObj}`, style)
+  }
+  // const errorData = { Message: error.message, Name: error.name, Code: error.code, Status: error.status }
+  // console.error('Message:', error.message)
+  // console.error('Name:', error.name)
+  // console.error('Code:', error.code)
+  // console.error('Status:', error.status)
+  // console.table(errorData)
 }
