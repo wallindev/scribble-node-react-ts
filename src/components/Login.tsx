f383d982e844b71504e61c56b1b4b67eed989385
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-26 16:26:56 +0100

    Built whole new Login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,35 +1,104 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-// import axios from 'axios'
-import type { FC, JSX } from 'react'
+import axios, { isAxiosError } from 'axios'
+import type { ChangeEvent, FC, JSX, KeyboardEvent, KeyboardEventHandler } from 'react'
+import type { AxiosError, AxiosResponse } from 'axios'
 import Layout from './layout/Layout'
-import AuthForm from './shared/AuthForm'
-import type { IGlobal } from '../types/general.types'
+import TextInput from './shared/TextInput'
+import CustomButton from './shared/CustomButton'
+import FlashMessage from './shared/FlashMessage'
+import type { IGlobal, TFlashMessage } from '../types/general.types'
+import { consoleError, dismissFlashMessage } from '../utils/functions'
+import { defaultFlashMessage } from '../utils/defaults'
 import { login } from '../utils/functions'
 
 const Login: FC<IGlobal> = ({ loading, theme, setTheme }): JSX.Element => {
   const navigate = useNavigate()
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [flashMessage, setFlashMessage] = useState<TFlashMessage>(defaultFlashMessage)
 
-  const handleLogin = async (username: string, password: string): Promise<void> => {
-    // try {
-    //   const response: AxiosResponse = await axios.post('/api/login', { email, password })
-    //   localStorage.setItem('token', response.data.token) // Store the token
-    //   navigate('/home') // Redirect to the user home page
-    // } catch (error) {
-    //   console.error('Login error:', error)
-    //   // Handle error (e.g., display a message)
-    // }
-    if (username === 'mikael' && password === 'grunge' ||
-      username === 'lisbeth' && password === 'larsson') {
-      login()
-      navigate('/home')
-    } else {
-      alert('Invalid credentials')
+  const handleLogin = async () => {
+    let error
+    try {
+      const response: AxiosResponse = await axios.post('/login', { email, password })
+      const { jwtToken, userId } = response.data
+      login(jwtToken, userId)
+    } catch (e) {
+      if (isAxiosError(e))  {
+        error = e as AxiosError
+        consoleError(error)
+        setFlashMessage({
+          message: 'An error occured trying to log in',
+          type: 'error',
+          visible: true,
+        })
+      } else {
+        console.error("Error trying to log in:\n", error)
+        setFlashMessage({
+          message: `Error trying to log in:<br />${error}`,
+          type: 'error',
+          visible: true,
+        })
+      }
+    }
+    if (!error) {
+      setTimeout(() => {
+        navigate('/home')
+      }, 3000)
+      setFlashMessage({
+        message: 'Login successful! Redirecting in 3 seconds...',
+        type: 'success',
+        visible: true,
+      })
+    }
+  }
+
+  const keyDownOnElement: KeyboardEventHandler = (key: KeyboardEvent<HTMLInputElement>) => {
+    if (key.code.toUpperCase() === 'ENTER' || key.code.toUpperCase() === 'NUMPADENTER') {
+      key.preventDefault()
+      handleLogin()
     }
   }
 
   return (
     <Layout loading={loading} theme={theme} setTheme={setTheme}>
-      <AuthForm formType="login" onSubmit={handleLogin} />
+      <div>
+        {flashMessage.visible && (
+          <FlashMessage
+            message={flashMessage.message}
+            type={flashMessage.type}
+            onDismiss={() => dismissFlashMessage(flashMessage, setFlashMessage)}
+          />
+        )}
+        <h3 className="text-2xl font-bold mb-4">Login</h3>
+        <div className="flex flex-col sm:items-start mb-4">
+          <label htmlFor="email" className="p-1">Email</label>
+          <TextInput
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+            onKeyDown={keyDownOnElement}
+          />
+          <label htmlFor="password" className="p-1">Password</label>
+          <TextInput
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+            onKeyDown={keyDownOnElement}
+          />
+          <div className="flex flex-row sm:items-start mt-4">
+            <CustomButton className="max-sm:flex-1/2 mr-0.5 sm:mr-1" type="button" onClick={() => navigate('/')}>&laquo; Cancel</CustomButton>
+            <CustomButton className="max-sm:flex-1/2 ml-0.5 sm:ml-1" type="button" onClick={handleLogin}>Login</CustomButton>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
