3947e2629f54683edfe0e9186d374a87fe565079
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-26 16:32:28 +0100

    Built whole new Register component

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,31 +1,139 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-// import axios from 'axios'
-import type { FC, JSX } from 'react'
+import axios, { isAxiosError } from 'axios'
+import type { ChangeEvent, FC, JSX, KeyboardEvent, KeyboardEventHandler } from 'react'
+import type { AxiosError, AxiosResponse } from 'axios'
 import Layout from './layout/Layout'
-import AuthForm from './shared/AuthForm'
-import type { IGlobal } from '../types/general.types'
-import { setAuth } from '../utils/functions'
+import TextInput from './shared/TextInput'
+import CustomButton from './shared/CustomButton'
+import FlashMessage from './shared/FlashMessage'
+import type { IGlobal, TFlashMessage } from '../types/general.types'
+import { consoleError, dismissFlashMessage } from '../utils/functions'
+import { defaultFlashMessage } from '../utils/defaults'
+import { login } from '../utils/functions'
 
 const Register: FC<IGlobal> = ({ loading, theme, setTheme }): JSX.Element => {
   const navigate = useNavigate()
+  const [firstName, setFirstName] = useState<string>('')
+  const [lastName, setLastName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('')
+  const [flashMessage, setFlashMessage] = useState<TFlashMessage>(defaultFlashMessage)
 
-  const handleRegister = (username: string, password: string): void => {
-    // try {
-    //   const response = await axios.post('/api/login', { username, password }) // Your Laravel login route
-    //   localStorage.setItem('token', response.data.token) // Store the token
-    //   '/' // Redirect to the article list
-    // } catch (error) {
-    //   console.error('Register error:', error)
-    //   // Handle error (e.g., display a message)
-    // }
-    console.log("Registering user:", username, password) // Log the values (or make a real API call)
-    setAuth(true)
-    navigate('/home')
+  const handleRegister = async () => {
+    let error
+    try {
+      const response: AxiosResponse = await axios.post('/register', { firstName, lastName, email, password, passwordConfirm })
+      // const { newUser, userId, jwtToken } = response.data
+      const { userId, jwtToken } = response.data
+      login(jwtToken, userId)
+    } catch (e) {
+      if (isAxiosError(e)) {
+        error = e as AxiosError
+        consoleError(error)
+        setFlashMessage({
+          message: 'An error occured trying to log in',
+          type: 'error',
+          visible: true,
+        })
+      } else {
+        console.error("Error trying to log in:\n", error)
+        setFlashMessage({
+          message: `Error trying to log in:<br />${error}`,
+          type: 'error',
+          visible: true,
+        })
+      }
+    }
+    if (!error) {
+      // TODO: Add sending a verify email, and building 'verify' endpoint
+      setTimeout(() => {
+        navigate('/home')
+      }, 3000)
+      setFlashMessage({
+        message: 'Regitration successful! Redirecting in 3 seconds...',
+        type: 'success',
+        visible: true,
+      })
+    }
+  }
+
+  const keyDownOnElement: KeyboardEventHandler = (key: KeyboardEvent<HTMLInputElement>) => {
+    if (key.code.toUpperCase() === 'ENTER' || key.code.toUpperCase() === 'NUMPADENTER') {
+      key.preventDefault()
+      handleRegister()
+    }
   }
 
   return (
     <Layout loading={loading} theme={theme} setTheme={setTheme}>
-      <AuthForm formType="register" onSubmit={handleRegister} />
+      <div>
+        {flashMessage.visible && (
+          <FlashMessage
+            message={flashMessage.message}
+            type={flashMessage.type}
+            onDismiss={() => dismissFlashMessage(flashMessage, setFlashMessage)}
+          />
+        )}
+        <h3 className="text-2xl font-bold mb-4">Register</h3>
+        <div className="flex flex-col sm:items-start mb-4">
+          <label htmlFor="firstName" className="p-1">First Name</label>
+          <TextInput
+            id="firstName"
+            name="firstName"
+            type="text"
+            value={firstName}
+            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
+            onKeyDown={keyDownOnElement}
+          />
+          <label htmlFor="lastName" className="p-1">Last Name</label>
+          <TextInput
+            id="lastName"
+            name="lastName"
+            type="text"
+            value={lastName}
+            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
+            onKeyDown={keyDownOnElement}
+          />
+          <label htmlFor="email" className="p-1">Email</label>
+          <TextInput
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+            onKeyDown={keyDownOnElement}
+          />
+          <label htmlFor="password" className="p-1">Password</label>
+          <TextInput
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+            onKeyDown={keyDownOnElement}
+          />
+          <label htmlFor="passwordConfirm" className="p-1">Confirm Password</label>
+          <TextInput
+            id="passwordConfirm"
+            name="passwordConfirm"
+            type="password"
+            value={passwordConfirm}
+            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setPasswordConfirm(e.target.value)}
+            onKeyDown={keyDownOnElement}
+          />
+          <div className="flex flex-row sm:items-start mt-4">
+            <CustomButton className="max-sm:flex-1/2 mr-0.5 sm:mr-1" type="button" onClick={() => navigate('/')}>&laquo; Cancel</CustomButton>
+            <CustomButton className="max-sm:flex-1/2 ml-0.5 sm:ml-1" type="button" onClick={handleRegister}>Register</CustomButton>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
