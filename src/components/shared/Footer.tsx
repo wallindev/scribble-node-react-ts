5a3412599cfea10dae84ad8d99b719cdbd0f87ed
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-27 16:44:37 +0100

    Changed link color on copyright text

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: FC<IFooter> = ({ theme, setTheme }): JSX.Element => {
   return (
     <div className="my-0 sm:mt-4 mx-auto p-2 pb-0 sm:p-4 sm:pb-0 sm:border-t border-main-content-bg">
       <div className="flex justify-between items-center text-xs">
-        <div>&copy; {(new Date).getUTCFullYear()} <TextLink to="mailto: wallindev@gmail.com">wallindev</TextLink></div>
+        <div>&copy; {(new Date).getUTCFullYear()} <TextLink className="text-text visited:text-text hover:text-text" to="mailto: wallindev@gmail.com">wallindev</TextLink></div>
         <div><ThemeSelector theme={theme} setTheme={setTheme} /></div>
       </div>
     </div>
