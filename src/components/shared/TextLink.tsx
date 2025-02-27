4ec70d0b32abda50e073ccb37adde8cbf0be2f0b
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-27 16:46:24 +0100

    Added classnames package to fix problem with missing css classes

--- src/components/shared/TextLink.tsx
+++ src/components/shared/TextLink.tsx
@@ -1,13 +1,14 @@
 import type { FC, JSX } from 'react'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 import type { ITextLink } from '../../types/general.types'
 
 const TextLink: FC<ITextLink> = ({ className, to, children, style, ...props }): JSX.Element => {
-  let classNames = 'text-link visited:text-link hover:no-underline hover:text-link--hover'
-  className ? `${classNames} ${className}` : className
+  let classes = 'text-link visited:text-link hover:no-underline hover:text-link--hover'
+  // className ? `${classNames} ${className}` : className
 
   return (
-    <Link className={classNames} to={to} style={style} {...props}>
+    <Link className={classNames(classes, className)} to={to} style={style} {...props}>
       {children}
     </Link>
   )
