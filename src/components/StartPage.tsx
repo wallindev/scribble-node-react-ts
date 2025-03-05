fbce92b8c117cb28f60d14cf1b33f85384884155
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-05 20:22:03 +0100

    Changed text, added link to articles page in text, swapped login and register buttons for text links

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,7 +1,6 @@
 import type { FC, JSX } from 'react'
 import Layout from './layout/Layout'
 import TextLink from './shared/TextLink'
-import CustomButton from './shared/CustomButton'
 import type { IGlobal } from '../types/general.types'
 import { isAuthenticated } from '../utils/functions'
 
@@ -11,13 +10,12 @@ const StartPage: FC<IGlobal> = ({ loading, theme, setTheme }): JSX.Element => {
       <div>
         <h1 className="text-2xl font-bold">Welcome to Scribble!</h1>
         <div className="mt-4">
-          <p className="mt-4">Scribble! - a tiny web app where you can create and manage your articles.</p>
+          <p className="mt-4">A tiny web app for creating and managing your
+             {!isAuthenticated() && <span className="ml-1">articles</span>}
+             {isAuthenticated() && <TextLink className="ml-1" to="/articles">articles</TextLink>}.
+          </p>
           <p className="mt-2">Join our community and share your thoughts with the world!</p>
         </div>
-        {/* !isAuthenticated() && <div className="flex flex-row sm:items-start mt-4">
-          <CustomButton to="/login" className="max-sm:flex-1/2 mr-0.5 sm:mr-1" type="button">Log In</CustomButton>
-          <CustomButton to="/register" className="max-sm:flex-1/2 ml-0.5 sm:ml-1" type="button">Register</CustomButton>
-        </div> */}
         {!isAuthenticated() && <div className="flex flex-row sm:items-start mt-4">
           <TextLink className="max-sm:flex-1/2 mr-0.5 sm:mr-1" to="/login">Log In</TextLink>
           <TextLink className="max-sm:flex-1/2 ml-0.5 sm:ml-1" to="/register">Register</TextLink>
