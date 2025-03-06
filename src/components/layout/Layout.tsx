1d34fdef1843fd2643c21c4e31bb60cd3802c4ea
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-06 20:27:45 +0100

    Tweaked and cleaned up fade-in effect

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef/* , useState */ } from 'react'
 import { useLocation } from 'react-router-dom'
 import type { FC, JSX } from 'react'
-import LoadText from '../shared/LoadText'
+// import LoadText from '../shared/LoadText'
 import MainNav from '../shared/MainNav'
 import Footer from '../shared/Footer'
 import { Theme } from '../../types/general.types'
 import type { ILayout } from '../../types/general.types'
 
-const Layout: FC<ILayout> = ({ loading, theme, setTheme, children }): JSX.Element => {
-  const [isVisible, setIsVisible] = useState(true)
+const Layout: FC<ILayout> = ({ /* loading,  */theme, setTheme, children }): JSX.Element => {
+  // const [isVisible, setIsVisible] = useState(true)
   const location = useLocation()
+  const divRef = useRef<HTMLDivElement>(null)
 
   // Fade-in-fade-out effect on route change (together with CSS transition)
   useEffect(() => {
-    setIsVisible(false)
-    const timeoutId = setTimeout(() => {
-      setIsVisible(true)
-    }, 100)
+    // setIsVisible(false)
+    const opacityTimer = setTimeout(() => {
+      // setIsVisible(true)
+      (divRef.current as HTMLDivElement).classList.replace('opacity-0', 'opacity-100')
+    }, 1)
 
-    return () => clearTimeout(timeoutId)
-  }, [location.key])
+    return () => clearTimeout(opacityTimer)
+  })
 
-  return loading ? <LoadText /> : (
-    <div style={{ transition: 'opacity 0.100s ease-in-out', opacity: isVisible ? 1 : 0 }}>
+  return /* loading ? <LoadText /> :  */(
+    <div ref={divRef} className="transition-opacity delay-0 duration-700 opacity-0">
       <div data-theme={Theme[theme!]} className="m-0 sm:mx-auto p-2 sm:p-4 w-screen sm:max-w-160 max-sm:h-screen flex flex-col bg-content-bg rounded-xl">
         <header>
           <MainNav />
