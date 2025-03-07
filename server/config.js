6d9007ccfbcab3a5991d02c29a2b5c2d5db40889
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-07 21:05:06 +0100

    Added environment and run-time location constants/env variables plus special variable envFileVars that contains only the env variables from the .env file

--- server/config.js
+++ server/config.js
@@ -1,21 +1,39 @@
 import path from 'node:path'
-import env from 'dotenv'
+import dotenv from 'dotenv'
+import { __dirname } from './constants.js'
 
-let filePathEnv
-try {
-  filePathEnv = path.resolve(`./server/.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`)
-  !filePathEnv && console.error('Env file not found')
-  env.config({ path: filePathEnv })
-} catch (error) {
-  console.error(`Error reading or parsing '${filePathEnv}' file:\n${error}`)
+process.env.NODE_ENV ||= 'development'
+process.env.RUN_ENV ||= 'local'
+
+export const   DEV_ENV = process.env.NODE_ENV === 'development'
+export const  TEST_ENV = process.env.NODE_ENV === 'test'
+export const  PROD_ENV = process.env.NODE_ENV === 'production'
+export const LOCAL_ENV = process.env.RUN_ENV  === 'local'
+export const  LIVE_ENV = !LOCAL_ENV
+
+let envFileVars = {}
+if (LOCAL_ENV) {
+  let filePathEnv, result
+  try {
+    filePathEnv = path.join(__dirname, '.env')
+    if (filePathEnv) {
+      result = dotenv.config({ path: filePathEnv })
+    } else {
+      console.error('Env file not found')
+    }
+    envFileVars = result.parsed
+  } catch (error) {
+    console.error(`Error reading or parsing '${filePathEnv}' file:\n${error}`)
+  }
 }
+export default envFileVars
 
 // Implement this? Use httpOnly and secure cookies!
 // Axios interceptor
 /* axios.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token')
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
-  return config;
+  return config
 }) */
