29f3651d5c79f7d93174bc1d0f9e60930830ec13
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-10 05:26:16 +0100

    Cleaned up

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef/* , useState */ } from 'react'
-import { useLocation } from 'react-router-dom'
 import type { FC, JSX } from 'react'
 // import LoadText from '../shared/LoadText'
 import MainNav from '../shared/MainNav'
@@ -8,15 +7,11 @@ import { Theme } from '../../types/general.types'
 import type { ILayout } from '../../types/general.types'
 
 const Layout: FC<ILayout> = ({ /* loading,  */theme, setTheme, children }): JSX.Element => {
-  // const [isVisible, setIsVisible] = useState(true)
-  const location = useLocation()
   const divRef = useRef<HTMLDivElement>(null)
 
   // Fade-in-fade-out effect on route change (together with CSS transition)
   useEffect(() => {
-    // setIsVisible(false)
     const opacityTimer = setTimeout(() => {
-      // setIsVisible(true)
       (divRef.current as HTMLDivElement)?.classList?.replace('opacity-0', 'opacity-100')
     }, 1)
 
