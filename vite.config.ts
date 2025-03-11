c539bc866178aeff0124f6e5ea367449f6ce0409
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-11 11:03:38 +0100

    Completely new, starting over with minimal info, added proxy so that you do not have to think about url, just the endpoints

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,24 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindCss from '@tailwindcss/vite'
-import injectEnvToHtml from './plugins/injectEnvToHtml.ts'
 
-// https://vite.dev/config/
-/** @type {import('vite').UserConfig} */
-export default ({ mode = 'development' }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = {
-    ...process.env,
-    ...loadEnv(mode, process.cwd()),
-  }
-
-  return defineConfig({
-    mode: mode || 'development',
-    plugins: [
-      react(),
-      tailwindCss(),
-      injectEnvToHtml(),
-    ],
-    server: {
-      // hmr: false,
-      host: 'localhost', // 0.0.0.0 for all interfaces
-      port: 5000,
-      allowedHosts: [
-        '127.0.0.1',
-        '192.168.32.2',
-        'grunge',
-        'grungecorp',
-        'grunge.dev',
-        'grungecorp.dev',
-      ],
-      watch: {
-        ignored: ['**/api-data/*.*', './TODO.txt'],
+export default defineConfig({
+  plugins: [ react(), ],
+  // build: {
+  //   outDir: 'dist',
+  // },
+  server: {
+    // hmr: false,
+    // host: 'localhost', // 0.0.0.0 for all interfaces
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Local API during dev
+        changeOrigin: true,
       },
-    }
-  })
-}
+    },
+    port: 5000,
+    // allowedHosts: ['127.0.0.1', '192.168.32.2', 'grunge', 'grungecorp', 'grunge.dev', 'grungecorp.dev'],
+    watch: {
+      ignored: ['**/api-data/*.*', './TODO.txt'],
+    },
+  },
+})
