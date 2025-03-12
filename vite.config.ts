fb415ea31f34924028e01b2e154d8fd135a812fd
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-12 11:25:11 +0100

    Added tailwindCss plugin and types definition for TypeScript in Vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindCss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [ react(), ],
+  plugins: [
+    react(),
+    tailwindCss(),
+  ],
   // build: {
   //   outDir: 'dist',
   // },
