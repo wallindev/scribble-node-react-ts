1e4fc6b195a7d9d4edee824d91cd797f44032f11
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-13 14:38:48 +0100

    It worked using only the path, so keeping just that

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,1 @@
-// Let's first try with just the path
 export const API_PATH = '/api'
-
-// then, set the real URL. this should absolutely work
-// export const API_URL = 'https://node-react-ts-33315b5eaefa.herokuapp.com/api'
-
-// Local URL with PORT, causes Heroku to connect to
-// "remote source" (which is my local app),
-// since Heroku works without ports except for
-// the dynamic port the whole Express app is bound
-// to before it is started. after the app is started
-// all API URL:s etc should be without ports
-// export const API_URL = 'http://localhost:3000/api'
