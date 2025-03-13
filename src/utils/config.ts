866f0e6b80b3a33fab32e91325fab3e4eee6d140
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-13 14:42:56 +0100

    Setting only the path "/api" as baseUrl for axios turned out to be the absolute best, cleanest and most dynamical solution. it works both remotely on Heroku, and locally, and should the app move to another host, or just change domain, this setting does not ever need to be changed (as long as the api itself is configured to run at "/api")

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,13 +1,4 @@
 import axios from 'axios'
 import { API_PATH } from './constants'
 
-// const location = window.location
-// console.log('location:', location)
-// console.log('location.protocol:', location.protocol)
-// console.log('location.hostname:', location.hostname)
-// console.log('location.port:', location.port)
-
-// const fullUrl = `${location.protocol}//${location.hostname}${location.port ? `:${location.port}` : ''}`
-// console.log('fullUrl:', fullUrl)
-
 axios.defaults.baseURL = API_PATH
