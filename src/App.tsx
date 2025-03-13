0a5a026c712054aaa93b257fa0d4343d7e048d49
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-13 17:23:44 +0200

    Put it back together again and removed location and location.key,added verify route, added wrapperRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Navigate, Routes, Route, useLocation } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom'
+import { useEffect, useRef, useState  } from 'react'
 import type { FC, JSX } from 'react'
 import StartPage from './components/StartPage'
 import Login from './components/Login'
 import Register from './components/Register'
+import Verify from './components/Verify'
 import Home from './components/Home'
 import User from './components/User'
 import Articles from './components/Articles'
@@ -13,18 +14,9 @@ import { Theme } from './types/general.types'
 import './utils/config'
 
 const App: FC = (): JSX.Element => {
-  return (
-    <Router>
-      <RoutesWrapper />
-    </Router>
-  )
-}
-
-const RoutesWrapper: FC = (): JSX.Element => {
-  const location = useLocation()
   const [theme, setTheme] = useState<Theme>(Theme.Default)
-  // const [userId, setUserId] = useState<number>(0)
   const [loading, setLoading] = useState<boolean>(false)
+  const wrapperRef = useRef<HTMLInputElement>(null)
 
   // Check if user stored theme before
   useEffect(() => {
@@ -37,13 +29,16 @@ const RoutesWrapper: FC = (): JSX.Element => {
     setTheme,
     loading,
     setLoading,
+    wrapperRef
   }
 
   return (
-      <Routes location={location} key={location.key}>
+    <Router>
+      <Routes>
         <Route path="/" element={<StartPage {...globalProps} />} />
         <Route path="/login" element={<Login {...globalProps} />} />
         <Route path="/register" element={<Register {...globalProps} />} />
+        <Route path="/verify/:verifyToken" element={<Verify {...globalProps} />} />
         <Route element={<ProtectedRoute />}>
           <Route path="/home" element={<Home {...globalProps} />} />
           <Route path="/profile" element={<User {...globalProps} />} />
@@ -55,6 +50,7 @@ const RoutesWrapper: FC = (): JSX.Element => {
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+    </Router>
   )
 }
 
