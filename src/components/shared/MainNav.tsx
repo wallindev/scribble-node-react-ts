4d0f7bc6956761e85239314459c32764f5062493
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-15 00:44:02 +0200

    Cleaned up and optimized component, added stopPropagation in toggleMenu eventhandler to be able to use other onClick eventhandler on Layout component

--- src/components/shared/MainNav.tsx
+++ src/components/shared/MainNav.tsx
@@ -1,57 +1,45 @@
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
 import type { FC, JSX } from 'react'
+import { MouseEvent } from 'react'
+import DelayedLink from './DelayedLink'
 import Logout from './Logout'
+import type { IMainNav } from '../../types/general.types'
 import { isAuthenticated } from '../../utils/functions'
 
-const MainNav: FC = (): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false)
+const MainNav: FC<IMainNav> = ({ wrapperRef, subNavOpen, setSubNavOpen }): JSX.Element => {
 
-  const toggleMenu = (): void => {
-    setIsOpen(!isOpen)
+  const toggleMenu = (e: MouseEvent<HTMLDivElement>): void => {
+    setSubNavOpen!(!subNavOpen)
+    e.stopPropagation()
   }
 
+  const burgerLinesUtilClasses = 'w-6 h-0.5 bg-white mx-0 my-1'
+  const linkUtilClasses = 'block border border-t-0 border-main-content-bg px-6 py-4 sm:border-0 sm:inline-block sm:px-1 sm:py-2'
+  // class="border border-t-0 border-main-content-bg"
   return (
-    <nav className="mt-0 mb-2 sm:mb-4 mx-auto p-2 sm:p-4 sm:border-b border-main-content-bg">
+    <nav className="mb-2 sm:mb-4 p-4 sm:border-b border-main-content-bg">
       <div className="flex justify-between items-center">
-        <div className="text-xl font-bold text-text py-3">
-          <NavLink to="/" className={({ isActive }) => `${isActive && 'font-bold'} text-text visited:text-text hover:text-text--hover transition-colors duration-200`} title='To Start Page'>Scribble!</NavLink>
+        <div className="text-xl font-bold">
+          <DelayedLink wrapperRef={wrapperRef} to="/" title="To Start Page">Scribble!</DelayedLink>
         </div>
-        <div className="flex items-center">
-          {isAuthenticated() && <>
-            <div className="hamburger block cursor-pointer" onClick={toggleMenu}>
-              <div className="w-6 h-0.5 bg-white mx-0 my-1.5"></div>
-              <div className="w-6 h-0.5 bg-white mx-0 my-1.5"></div>
-              <div className="w-6 h-0.5 bg-white mx-0 my-1.5"></div>
-            </div>
-            <ul className={`${isOpen ? 'flex nav-open' : 'hidden nav-closed'} list-none flex-col absolute top-18 left-0 w-full bg-content-bg m-0 p-0 opacity-95 border border-main-content-bg`}>
-              <NavLink to="/home" className={({ isActive }) => `${isActive && 'font-bold'} text-text visited:text-text hover:text-text--hover transition-colors duration-200`}>
-                <li className="m-0 px-4 py-3 border border-t-0 border-main-content-bg">Home</li>
-              </NavLink>
-                <NavLink to="/articles" className={({ isActive }) => `${isActive && 'font-bold'} text-text visited:text-text hover:text-text--hover transition-colors duration-200`}>
-                  <li className="m-0 px-4 py-3 border border-t-0 border-main-content-bg">My Articles</li>
-                </NavLink>
-                <NavLink to="/profile" className={({ isActive }) => `${isActive && 'font-bold'} text-text visited:text-text hover:text-text--hover transition-colors duration-200`}>
-                  <li className="m-0 px-4 py-3 border border-t-0 border-main-content-bg">My Profile</li>
-                </NavLink>
-                <li className="flex flex-col items-end m-0 px-4 py-3 border border-t-0 border-main-content-bg"><Logout /></li>
-            </ul>
-            <ul className="hidden nav-large list-none">
-              <li className="inline-block mx-1 my-2">
-                <NavLink to="/home" className={({ isActive }) => `${isActive && 'font-bold'} text-text visited:text-text hover:text-text--hover transition-colors duration-200`}>Home</NavLink>
-              </li>
-              <li className="inline-block mx-1 my-2">
-                <NavLink to="/articles" className={({ isActive }) => `${isActive && 'font-bold'} text-text visited:text-text hover:text-text--hover transition-colors duration-200`}>My Articles</NavLink>
-              </li>
-              <li className="inline-block mx-1 my-2">
-                <NavLink to="/profile" className={({ isActive }) => `${isActive && 'font-bold'} text-text visited:text-text hover:text-text--hover transition-colors duration-200`}>My Profile</NavLink>
-              </li>
-              <li className="inline-block mx-1 mt-2 mb-3 ml-4">
-                <Logout size="small" />
-              </li>
-            </ul>
-          </>}
-        </div>
+        {isAuthenticated() && <div className="flex items-center">
+          <div className="block sm:hidden cursor-pointer" onClick={(e: MouseEvent<HTMLDivElement>) => toggleMenu(e)}>
+            <div className={burgerLinesUtilClasses}></div>
+            <div className={burgerLinesUtilClasses}></div>
+            <div className={burgerLinesUtilClasses}></div>
+          </div>
+          <ul className={`${subNavOpen ? 'flex flex-col' : 'hidden'} sm:hidden list-none w-full absolute top-16 left-0 bg-content-bg opacity-95`}>
+            <li><DelayedLink wrapperRef={wrapperRef} to="/home" className={linkUtilClasses.replace('border-t-0', '')} title="To Home My Page">Home</DelayedLink></li>
+            <li><DelayedLink wrapperRef={wrapperRef} to="/articles" className={linkUtilClasses} title="To My Articles Page">My Articles</DelayedLink></li>
+            <li><DelayedLink wrapperRef={wrapperRef} to="/profile" className={linkUtilClasses} title="To My Profile Page">My Profile</DelayedLink></li>
+            <li className={`${linkUtilClasses} flex flex-col items-end`}><Logout size="medium" /></li>
+          </ul>
+          <ul className="hidden sm:flex list-none">
+            <li><DelayedLink wrapperRef={wrapperRef} to="/home" className={linkUtilClasses} title="To Home My Page">Home</DelayedLink></li>
+            <li><DelayedLink wrapperRef={wrapperRef} to="/articles" className={linkUtilClasses} title="To My Articles Page">My Articles</DelayedLink></li>
+            <li><DelayedLink wrapperRef={wrapperRef} to="/profile" className={linkUtilClasses} title="To My Profile Page">My Profile</DelayedLink></li>
+            <li className="flex items-center"><Logout className="ml-2" size="small" title="Logout of Scribble!" /></li>
+          </ul>
+        </div>}
       </div>
     </nav>
   )
