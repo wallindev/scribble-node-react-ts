f6f7809d612fe6cd443f9e9b95b6cea433307ec8
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-15 00:46:14 +0200

    Added text-link utility class on active link pseudo class

--- src/components/shared/TextLink.tsx
+++ src/components/shared/TextLink.tsx
@@ -4,9 +4,8 @@ import classNames from 'classnames'
 import type { ITextLink } from '../../types/general.types'
 
 const TextLink: FC<ITextLink> = ({ className, to, children, style, ...props }): JSX.Element => {
-  let classes = 'text-link visited:text-link hover:no-underline hover:text-link--hover'
-  // className ? `${classNames} ${className}` : className
-
+  let classes = 'text-link visited:text-link active:text-link hover:no-underline hover:text-link-hover'
+  // class="bg-button-"
   return (
     <Link className={classNames(classes, className)} to={to} style={style} {...props}>
       {children}
