9c2fdb3f6ccbf216f4f88a11755d100776502885
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-14 19:15:18 +0200

    Swapped TextLink for DelayedLink, added wrapperRef, and re-organized some code

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,25 +1,28 @@
 import type { FC, JSX } from 'react'
 import Layout from './layout/Layout'
-import TextLink from './shared/TextLink'
+import DelayedLink from './shared/DelayedLink'
+import { LinkType } from '../types/general.types'
 import type { IGlobal } from '../types/general.types'
 import { isAuthenticated } from '../utils/functions'
 
-const StartPage: FC<IGlobal> = ({ loading, theme, setTheme }): JSX.Element => {
+const StartPage: FC<IGlobal> = ({ loading, theme, setTheme, wrapperRef }): JSX.Element => {
   return (
-    <Layout loading={loading} theme={theme} setTheme={setTheme}>
+    <Layout loading={loading} theme={theme} setTheme={setTheme} wrapperRef={wrapperRef}>
       <div>
         <h1 className="text-2xl font-bold">Welcome to Scribble!</h1>
         <div className="mt-4">
           <p className="mt-4">A tiny web app for creating and managing your
-             {!isAuthenticated() && <span className="ml-1">articles</span>}
-             {isAuthenticated() && <TextLink className="ml-1" to="/articles">articles</TextLink>}.
-          </p>
-          <p className="mt-2">Join our community and share your thoughts with the world!</p>
+            {isAuthenticated() ?
+              <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="ml-1" to="/articles">articles</DelayedLink> :
+              <span className="ml-1">articles</span>}
+          .</p>
+          <p className="mt-2">Register today and start scribbling away your thoughts and dreams!</p>
         </div>
-        {!isAuthenticated() && <div className="flex flex-row sm:items-start mt-4">
-          <TextLink className="max-sm:flex-1/2 mr-0.5 sm:mr-1" to="/login">Log In</TextLink>
-          <TextLink className="max-sm:flex-1/2 ml-0.5 sm:ml-1" to="/register">Register</TextLink>
-        </div>}
+        {!isAuthenticated() &&
+          <div className="flex flex-row sm:items-start mt-4">
+            <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="max-sm:flex-1/2 mr-0.5 sm:mr-1" to="/login">Log In</DelayedLink>
+            <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="max-sm:flex-1/2 ml-0.5 sm:ml-1" to="/register">Register</DelayedLink>
+          </div>}
       </div>
     </Layout>
   )
