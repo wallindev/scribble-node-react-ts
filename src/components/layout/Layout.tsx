2c54b28dae3381aa38d3e26f1478d496d53efb10
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-15 00:34:53 +0200

    Changed to using new constants FADE_IN_TIME and FADE_OUT_TIME, moved subNavOpen and setSubNavOpen here from MainNav to be able to use eventhandler to close subnav when user clicks outside of it, added wrapperRef, added and re-organized props

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,28 +1,38 @@
-import { useEffect, useRef/* , useState */ } from 'react'
+import { useEffect, useState } from 'react'
 import type { FC, JSX } from 'react'
 // import LoadText from '../shared/LoadText'
 import MainNav from '../shared/MainNav'
 import Footer from '../shared/Footer'
 import { Theme } from '../../types/general.types'
+import { FADE_IN_TIME, FADE_OUT_TIME } from '../../utils/constants'
 import type { ILayout } from '../../types/general.types'
 
-const Layout: FC<ILayout> = ({ /* loading,  */theme, setTheme, children }): JSX.Element => {
-  const divRef = useRef<HTMLDivElement>(null)
+const Layout: FC<ILayout> = ({ /* loading,  */theme, setTheme, wrapperRef, children }): JSX.Element => {
+  const [subNavOpen, setSubNavOpen] = useState(false)
 
   // Fade-in-fade-out effect on route change (together with CSS transition)
   useEffect(() => {
-    const opacityTimer = setTimeout(() => {
-      (divRef.current as HTMLDivElement)?.classList?.replace('opacity-0', 'opacity-100')
-    }, 1)
+    const divWrapper = wrapperRef.current as HTMLDivElement
 
-    return () => clearTimeout(opacityTimer)
+    // Initiate fade-in
+    const fadeInTimer = setTimeout(() => {
+      divWrapper.classList?.replace(`duration-${FADE_OUT_TIME}`, `duration-${FADE_IN_TIME}`)
+      divWrapper.classList?.replace('opacity-0', 'opacity-100')
+    }, 100)
+
+    return () => {
+      // Initiate fade-out (moved to DelayedLink component!)
+      // divWrapper.classList?.replace('opacity-100', 'opacity-0')
+
+      clearTimeout(fadeInTimer)
+    }
   }, [])
 
   return /* loading ? <LoadText /> :  */(
-    <div ref={divRef} className="transition-opacity delay-0 duration-700 opacity-0">
-      <div data-theme={Theme[theme!]} className="m-0 sm:mx-auto p-2 sm:p-4 w-screen sm:max-w-160 max-sm:h-screen flex flex-col bg-content-bg rounded-xl">
+    <div ref={wrapperRef} className={`transition-all delay-0 duration-${FADE_IN_TIME} opacity-0`} onClick={() => setSubNavOpen!(false)} data-theme={Theme[theme!]}>
+      <div className="m-0 sm:mx-auto p-2 sm:p-4 w-screen sm:max-w-160 max-sm:h-screen flex flex-col bg-content-bg rounded-xl">
         <header>
-          <MainNav />
+          <MainNav wrapperRef={wrapperRef} subNavOpen={subNavOpen} setSubNavOpen={setSubNavOpen} />
         </header>
         <main className="flex-1 p-4 sm:p-4 bg-main-content-bg rounded-xl overflow-y-auto">
           {children}
