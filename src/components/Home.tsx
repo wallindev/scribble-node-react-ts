19d79de08c51462e1f364ec412082325d624fb18
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-14 18:13:22 +0200

    Added wrapperRef, swapped Textlink for DelayedLink

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,19 @@
 import type { FC, JSX } from 'react'
 import Layout from './layout/Layout'
-// import Logout from './shared/Logout'
-import TextLink from './shared/TextLink'
+import DelayedLink from './shared/DelayedLink'
+import { LinkType } from '../types/general.types'
 import type { IGlobal } from '../types/general.types'
 
-const Home: FC<IGlobal> = ({ loading, theme, setTheme }): JSX.Element => {
+const Home: FC<IGlobal> = ({ loading, theme, setTheme, wrapperRef }): JSX.Element => {
   return (
-    <Layout loading={loading} theme={theme} setTheme={setTheme}>
+    <Layout loading={loading} theme={theme} setTheme={setTheme} wrapperRef={wrapperRef}>
       <div>
         <h1 className="text-2xl font-bold mb-4">Welcome to Your Home Page!</h1>
-        <p className="mb-2">Here you can manage your <TextLink to="/articles">Articles</TextLink> and your <TextLink to="/profile">Profile</TextLink>.</p>
+        <p className="mb-2">
+          Here you can manage your
+          <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="ml-1" to="/articles">Articles</DelayedLink> and your
+          <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="ml-1" to="/profile">Profile</DelayedLink>.
+        </p>
         <p className="mb-1">Scribble away, and save all your thoughts and ideas in one place!</p>
       </div>
     </Layout>
