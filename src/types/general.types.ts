94b09823940ca3e467746d87ea478afa610b2c6f
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-17 18:44:38 +0200

    Added LinkType enum, IMainNav and IDelayedLink interfaces, wrapperRef prop in IFlashMessage interface, and all global props in IGlobal interface

--- src/types/general.types.ts
+++ src/types/general.types.ts
@@ -1,4 +1,4 @@
-import type { CSSProperties, Dispatch, MouseEventHandler, ReactNode, SetStateAction } from 'react'
+import type { CSSProperties, Dispatch, MouseEventHandler, ReactNode, RefObject, SetStateAction } from 'react'
 
 /*
  * Enums
@@ -22,6 +22,12 @@ export enum Theme {
   Pink,
   Purple,
   Gray
+}
+
+export enum LinkType {
+  Nav,
+  Button,
+  Text
 }
 
 /*
@@ -59,6 +65,13 @@ export type TFlashMessage = {
  * Interfaces (props)
  *
  */
+
+export interface IMainNav {
+  wrapperRef: RefObject<HTMLDivElement | null>
+  subNavOpen?: boolean
+  setSubNavOpen?: Dispatch<SetStateAction<boolean>>
+}
+
 export interface IFlashMessage {
   message: string
   type: 'success' | 'info' | 'warning' | 'error'
@@ -75,15 +88,35 @@ export interface IGlobal {
   setTheme: Dispatch<SetStateAction<Theme>>
   loading: boolean
   setLoading?: Dispatch<SetStateAction<boolean>>
+  wrapperRef: RefObject<HTMLDivElement | null>
 }
 
 export interface ILayout extends IGlobal {
+  theme: Theme
+  setTheme: Dispatch<SetStateAction<Theme>>
+  loading: boolean
+  setLoading?: Dispatch<SetStateAction<boolean>>
+  wrapperRef: RefObject<HTMLDivElement | null>
   children: ReactNode
 }
 
 export type ILoadText = {
   loading?: boolean
   defaultText?: string
+}
+
+export interface IDelayedLink {
+  wrapperRef: RefObject<HTMLDivElement | null>
+  linkType?: LinkType
+  delay?: number
+  className?: string | (({ isActive }: { isActive: any }) => string)
+  buttonType?: 'button' | 'submit' | 'reset'
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  size?: 'small' | 'medium' | 'large'
+  style?: CSSProperties
+  to?: string
+  children: ReactNode
+  [key: string]: any
 }
 
 export interface ITextLink {
