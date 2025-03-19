92c7c98b83fc99a80249a42901bef21115feca8f
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-20 00:14:36 +0200

    Moved all constants to constants.js, and cleaned up file

--- server/config.js
+++ server/config.js
@@ -1,23 +1,6 @@
 import path from 'node:path'
 import dotenv from 'dotenv'
-import { __dirname } from './constants.js'
-
-/*
- * These are set in the different npm run-scripts
- * after the principle that everything is run
- * live/remotely and in production unless
- * otherwise stated. That way, there is
- * less and less clutter the closer
- * we are to the finished app.
- */
-export const NODE_ENV = process.env.NODE_ENV || 'production'
-export const RUN_ENV = process.env.RUN_ENV  || 'live'
-
-export const   DEV_ENV = process.env.NODE_ENV === 'development'
-export const  TEST_ENV = process.env.NODE_ENV === 'test'
-export const  PROD_ENV = process.env.NODE_ENV === 'production'
-export const  IS_LOCAL = process.env.RUN_ENV  === 'local'
-export const   IS_LIVE = !IS_LOCAL
+import { __dirname, IS_LIVE, IS_LOCAL } from './constants.js'
 
 export let envFileVars = {}
 if (IS_LOCAL) {
@@ -37,18 +20,8 @@ if (IS_LOCAL) {
 
 export let envConfigVars = {}
 if (IS_LIVE) {
-  envConfigVars['CSS_PATH']   = process.env.CSS_PATH   || '/css/index.css'
-  envConfigVars['DB_PATH']    = process.env.DB_PATH    || './api-data/db.json'
+  envConfigVars['CSS_PATH']    = process.env.CSS_PATH    || '/css/index.css'
+  envConfigVars['DB_PATH']     = process.env.DB_PATH     || './api-data/db.json'
   envConfigVars['SECRET_AUTH'] = process.env.SECRET_AUTH || 'b029f401e21027eb9ddb4204f6aad6923c192ea2323b2bbb96d031c2c0a18051'
-  envConfigVars['PORT']       = process.env.PORT       || 3000
+  envConfigVars['PORT']        = process.env.PORT        || 3000
 }
-
-// Implement this? Use httpOnly and secure cookies!
-// Axios interceptor
-/* axios.interceptors.request.use(config => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
-}) */
