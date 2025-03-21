fb6d5a00aa7502c6e193e679aaf1229600b80042
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-22 00:37:19 +0200

    Removed flashMessage and setFlashMessage and added to props instead, removed all JSX that was connected to FlashMessage

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -6,19 +6,17 @@ import type { AxiosError, AxiosResponse } from 'axios'
 import Layout from './layout/Layout'
 import TextInput from './shared/TextInput'
 import CustomButton from './shared/CustomButton'
-import FlashMessage from './shared/FlashMessage'
-import type { IGlobal, TFlashMessage } from '../types/general.types'
-import { consoleError, dismissFlashMessage, scrollSmoothlyToTop } from '../utils/functions'
+import type { IGlobal } from '../types/general.types'
+import { consoleError, scrollSmoothlyToTop } from '../utils/functions'
 import { defaultFlashMessage } from '../utils/defaults'
 
-const Register: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, wrapperRef }): JSX.Element => {
+const Register: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessage, setFlashMessage, wrapperRef }): JSX.Element => {
   const navigate = useNavigate()
   const [firstName, setFirstName] = useState<string>('')
   const [lastName, setLastName] = useState<string>('')
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [passwordConfirm, setPasswordConfirm] = useState<string>('')
-  const [flashMessage, setFlashMessage] = useState<TFlashMessage>(defaultFlashMessage)
 
   const handleRegister = async () => {
     let error, userId
@@ -58,7 +56,7 @@ const Register: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, wrapperRe
       setFlashMessage(defaultFlashMessage)
       setTimeout(() => {
         setFlashMessage({
-          message: 'Registration successful! Check your mail that you registered with!',
+          message: 'Registration successful! Please check your inbox for an email verification link.',
           type: 'success',
           visible: true,
         })
@@ -74,69 +72,62 @@ const Register: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, wrapperRe
   }
 
   return (
-    <Layout loading={loading} theme={theme} setTheme={setTheme} wrapperRef={wrapperRef}>
-      <div>
-        {flashMessage.visible ? <FlashMessage
-            message={flashMessage.message}
-            type={flashMessage.type}
-            onDismiss={() => dismissFlashMessage(flashMessage, setFlashMessage)}
-          /> : <div className="h-8 mb-2"></div>}
-        <h3 className="text-2xl font-bold mb-4">Register</h3>
-        <div className="flex flex-col sm:items-start mb-4">
-          <label htmlFor="firstName" className="p-1">First Name</label>
-          <TextInput
-            id="firstName"
-            name="firstName"
-            type="text"
-            value={firstName}
-            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
-            onKeyDown={keyDownOnElement}
-          />
-          <label htmlFor="lastName" className="p-1">Last Name</label>
-          <TextInput
-            id="lastName"
-            name="lastName"
-            type="text"
-            value={lastName}
-            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
-            onKeyDown={keyDownOnElement}
-          />
-          <label htmlFor="email" className="p-1">Email</label>
-          <TextInput
-            id="email"
-            name="email"
-            type="email"
-            value={email}
-            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-            onKeyDown={keyDownOnElement}
-          />
-          <label htmlFor="password" className="p-1">Password</label>
-          <TextInput
-            id="password"
-            name="password"
-            type="password"
-            value={password}
-            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-            onKeyDown={keyDownOnElement}
-          />
-          <label htmlFor="passwordConfirm" className="p-1">Confirm Password</label>
-          <TextInput
-            id="passwordConfirm"
-            name="passwordConfirm"
-            type="password"
-            value={passwordConfirm}
-            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setPasswordConfirm(e.target.value)}
-            onKeyDown={keyDownOnElement}
-          />
-          <div className="flex flex-row sm:items-start mt-4">
-            <CustomButton className="max-sm:flex-1/2 mr-0.5 sm:mr-1" type="button" onClick={() => navigate('/')}>&laquo; Cancel</CustomButton>
-            <CustomButton className="max-sm:flex-1/2 ml-0.5 sm:ml-1" type="button" onClick={handleRegister}>Register</CustomButton>
-          </div>
+    <Layout loading={loading} theme={theme} setTheme={setTheme} flashMessage={flashMessage} setFlashMessage={setFlashMessage} wrapperRef={wrapperRef}>
+      <h3 className="text-2xl font-bold mb-4">Register</h3>
+      <div className="flex flex-col sm:items-start mb-4">
+        <label htmlFor="firstName" className="p-1">First Name</label>
+        <TextInput
+          id="firstName"
+          name="firstName"
+          type="text"
+          value={firstName}
+          className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
+          onKeyDown={keyDownOnElement}
+        />
+        <label htmlFor="lastName" className="p-1">Last Name</label>
+        <TextInput
+          id="lastName"
+          name="lastName"
+          type="text"
+          value={lastName}
+          className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
+          onKeyDown={keyDownOnElement}
+        />
+        <label htmlFor="email" className="p-1">Email</label>
+        <TextInput
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          onKeyDown={keyDownOnElement}
+        />
+        <label htmlFor="password" className="p-1">Password</label>
+        <TextInput
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          onKeyDown={keyDownOnElement}
+        />
+        <label htmlFor="passwordConfirm" className="p-1">Confirm Password</label>
+        <TextInput
+          id="passwordConfirm"
+          name="passwordConfirm"
+          type="password"
+          value={passwordConfirm}
+          className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setPasswordConfirm(e.target.value)}
+          onKeyDown={keyDownOnElement}
+        />
+        <div className="flex flex-row sm:items-start mt-4">
+          <CustomButton className="max-sm:flex-1/2 mr-0.5 sm:mr-1" type="button" onClick={() => navigate('/')}>&laquo; Cancel</CustomButton>
+          <CustomButton className="max-sm:flex-1/2 ml-0.5 sm:ml-1" type="button" onClick={handleRegister}>Register</CustomButton>
         </div>
       </div>
     </Layout>
