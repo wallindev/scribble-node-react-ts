f421af82f4c38e40d1847b7bb98585f48b1881e5
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-22 00:34:06 +0200

    Removed flashMessage and setFlashMessage and added to props instead, added defaultRequestConfig to api calls, moved out error handling in api calls to new error handling function handleHttpError, removed all JSX that was connected to FlashMessage

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,62 +6,67 @@ import type { AxiosError, AxiosResponse } from 'axios'
 import Layout from './layout/Layout'
 import TextInput from './shared/TextInput'
 import DelayedLink from './shared/DelayedLink'
-import FlashMessage from './shared/FlashMessage'
 import { LinkType } from '../types/general.types'
-import type { IGlobal, TFlashMessage } from '../types/general.types'
-import { consoleError, dismissFlashMessage, login } from '../utils/functions'
+import type { IGlobal } from '../types/general.types'
+import { consoleError, login, scrollSmoothlyToTop } from '../utils/functions'
 import { defaultFlashMessage } from '../utils/defaults'
 
-const Login: FC<IGlobal> = ({ loading, theme, setTheme, wrapperRef }): JSX.Element => {
+const Login: FC<IGlobal> = ({ loading, theme, setTheme, flashMessage, setFlashMessage, wrapperRef }): JSX.Element => {
   const navigate = useNavigate()
   const inputEmailRef = useRef<HTMLInputElement>(null)
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
-  const [flashMessage, setFlashMessage] = useState<TFlashMessage>(defaultFlashMessage)
 
   useEffect(() => {
     (inputEmailRef.current as HTMLInputElement).focus()
   }, [])
 
   const handleLogin = async () => {
-    let error, tokenData
+    let httpError, tokenData
     try {
       const response: AxiosResponse = await axios.post('/login', { email, password })
       if (response.status === HttpStatusCode.Ok && response.data) {
         const { userId, authToken, issued, expires } = response.data
+        // console.log('userId:', userId)
+        // console.log('authToken:', authToken)
+        // console.log('issued:', issued)
+        // console.log('expires:', expires)
+        if (!userId || !authToken || !issued || !expires) throw new Error("Missing Response data content")
         tokenData = JSON.stringify({ userId, authToken, issued, expires })
         // console.log('tokenData:', tokenData)
         // console.log('JSON.parse(tokenData):', JSON.parse(tokenData))
         login(tokenData)
       }
-    } catch (e) {
-      if (isAxiosError(e))  {
-        error = e as AxiosError
-        consoleError(error)
+    } catch (error) {
+      if (isAxiosError(error))  {
+        httpError = error as AxiosError
+        consoleError(httpError)
         setFlashMessage({
           message: 'An error occured trying to log in',
           type: 'error',
           visible: true,
         })
       } else {
-        error = e
-        console.error("Error trying to log in:\n", error)
+        httpError = error as Error
+        console.error("Error trying to log in:", error)
         setFlashMessage({
-          message: `Error trying to log in:<br />${error}`,
+          message: `Error trying to log in: ${error}`,
           type: 'error',
           visible: true,
         })
       }
     }
-    if (!error && tokenData) {
-      setTimeout(() => {
-        navigate('/home')
-      }, 3000)
+    if (!httpError && tokenData) {
+      scrollSmoothlyToTop()
       setFlashMessage({
-        message: 'Login successful! Redirecting in 3 seconds...',
+        message: 'Login successful! Redirecting...',
         type: 'success',
         visible: true,
       })
+      setTimeout(() => {
+        navigate('/home')
+        setFlashMessage(defaultFlashMessage)
+      }, 3000)
     }
   }
 
@@ -73,40 +78,33 @@ const Login: FC<IGlobal> = ({ loading, theme, setTheme, wrapperRef }): JSX.Eleme
   }
 
   return (
-    <Layout loading={loading} theme={theme} setTheme={setTheme} wrapperRef={wrapperRef}>
-      <div>
-        {flashMessage.visible ? <FlashMessage
-            message={flashMessage.message}
-            type={flashMessage.type}
-            onDismiss={() => dismissFlashMessage(flashMessage, setFlashMessage)}
-          /> : <div className="h-8 mb-2"></div>}
-        <h3 className="text-2xl font-bold mb-4">Login</h3>
-        <div className="flex flex-col sm:items-start mb-4">
-          <label htmlFor="email" className="p-1">Email</label>
-          <TextInput
-            id="email"
-            name="email"
-            type="email"
-            value={email}
-            ref={inputEmailRef}
-            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-            onKeyDown={keyDownOnElement}
-          />
-          <label htmlFor="password" className="p-1">Password</label>
-          <TextInput
-            id="password"
-            name="password"
-            type="password"
-            value={password}
-            className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-            onKeyDown={keyDownOnElement}
-          />
-          <div className="flex flex-row sm:items-start mt-4">
-            <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Button} className="max-sm:flex-1/2 mr-0.5 sm:mr-1" buttonType="button" to="/">&laquo; Cancel</DelayedLink>
-            <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Button} className="max-sm:flex-1/2 ml-0.5 sm:ml-1" type="button" onClick={handleLogin}>Login</DelayedLink>
-          </div>
+    <Layout loading={loading} theme={theme} setTheme={setTheme} flashMessage={flashMessage} setFlashMessage={setFlashMessage} wrapperRef={wrapperRef}>
+      <h3 className="text-2xl font-bold mb-4">Login</h3>
+      <div className="flex flex-col sm:items-start mb-4">
+        <label htmlFor="email" className="p-1">Email</label>
+        <TextInput
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          ref={inputEmailRef}
+          className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          onKeyDown={keyDownOnElement}
+        />
+        <label htmlFor="password" className="p-1">Password</label>
+        <TextInput
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          className="w-full sm:w-8/10 inset-shadow-[2px_2px_5px_rgba(0,0,0,0.3)] p-2 text-xl mb-4 border-0 outline-0"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          onKeyDown={keyDownOnElement}
+        />
+        <div className="flex flex-row sm:items-start mt-4">
+          <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Button} className="max-sm:flex-1/2 mr-0.5 sm:mr-1" buttonType="button" to="/">&laquo; Cancel</DelayedLink>
+          <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Button} className="max-sm:flex-1/2 ml-0.5 sm:ml-1" type="button" onClick={handleLogin}>Login</DelayedLink>
         </div>
       </div>
     </Layout>
