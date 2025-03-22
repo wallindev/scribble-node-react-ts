d61d1a990dee4e829c6db4fda9167afa0be1b37b
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-23 00:48:54 +0200

    Will perhaps use interceptor instead of default request config object

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,13 @@
 import axios from 'axios'
+// import type { AxiosRequestConfig } from 'axios'
 import { API_PATH } from './constants'
+// import { getAuthToken } from './functions'
 
 axios.defaults.baseURL = API_PATH
+
+// Implement this? Use httpOnly and secure cookies!
+// Axios interceptor
+// axios.interceptors.request.use(config => {
+//   config.headers.Authorization = `Bearer ${getAuthToken()}`
+//   return config
+// })
