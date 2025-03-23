f9654f4996d9d0a3dc0127cd50cdedcb1aca7647
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-24 00:55:06 +0200

    Added getUserEmail and hasUserEmail functions, and handleHttpError function for the new centralized error handling, added authentication flow for server authentication

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,9 @@
 // import { useState } from 'react'
+import axios, { isAxiosError } from 'axios'
 import type { Dispatch, SetStateAction } from 'react'
-import type { TFlashMessage } from '../types/general.types'
-import { isAxiosError } from 'axios'
-import type { AxiosError } from 'axios'
+import type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import { TFlashMessage, TokenType } from '../types/general.types'
+import type { NavigateFunction } from 'react-router-dom'
 
 // Example of JSDoc
 /**
@@ -64,6 +65,16 @@ export const getUserId = (): number | null => {
 
 export const hasUserId = (): boolean => getUserId() !== null
 
+export const getUserEmail = (): string | null => {
+  const tokenData = getTokenData()
+  if (!tokenData) return null
+  const tokenDataJson = JSON.parse(tokenData)
+  if (!tokenDataJson.userEmail) return null
+  return tokenDataJson.userEmail
+}
+
+export const hasUserEmail = (): boolean => getUserEmail() !== null
+
 export const getAuthToken = (): string | null => {
   const tokenData = getTokenData()
   if (!tokenData) return null
@@ -87,12 +98,126 @@ export const authTokenValid = (): boolean => {
 
 // Helper function to check authentication status
 // TODO: Change this to server-only cookie
-export const isAuthenticated = (): boolean => hasUserId() && hasAuthToken() && authTokenValid()
+export const isAuthenticated = (setAuth: Dispatch<SetStateAction<boolean>>, defaultRequestConfig?: AxiosRequestConfig | {}): boolean | void => {
+  /*
+   * Client authentication
+   *
+   */
+  if (hasUserId() && hasAuthToken() && authTokenValid()) {
+    setAuth(true)
+  } else {
+    setAuth(false)
+  }
+
+  /*
+   * Server authentication
+   *
+   */
+  /* // console.log('defaultRequestConfig:', defaultRequestConfig)
+  if (!defaultRequestConfig) setAuth(false)
+
+  axios.get('/token', defaultRequestConfig)
+  .then(res => {
+    // console.log('response:', res)
+    if (!res.data?.authenticated) setAuth(false)
+    setAuth(res.data.authenticated)
+  })
+  .catch(error => {
+    let httpError
+    setAuth(false)
+    handleHttpError(httpError, error)
+  }) */
+}
 
 export const localDateStr = (dateStr?: string | number | Date | null): string => {
   const date : Date = dateStr ? new Date(dateStr) : new Date()
   return date.toLocaleString("sv-SE", { timeZone: "Europe/Stockholm" })
 }
+
+export const handleHttpError = (httpError: any, error: any, setFlashMessage?: Dispatch<SetStateAction<TFlashMessage>>, defaultFlashMessage?: any, tokenType?: TokenType, navigate?: NavigateFunction) => {
+  if (isAxiosError(error)) {
+    httpError = error as AxiosError
+    const nestedError = (httpError.response as AxiosResponse)?.data?.error
+    if (nestedError) {
+      console.log('nestedError.name:', nestedError.name)
+      console.log('nestedError.message:', nestedError.message)
+      // console.log('nestedError.stack:', nestedError.stack)
+      // console.error('nestedError:', nestedError)
+      if (nestedError.name === 'TokenExpiredError') {
+        console.error('[Nested] TokenExpiredError: Token expired:', nestedError)
+        console.log('nestedError.expiredAt:', localDateStr(nestedError.expiredAt))
+        if (setFlashMessage) {
+          const expMsg = tokenType === TokenType.Auth ? 'Session has expired. Logging out...' : 'Email verification token has expired. Try re-sending the link.'
+          setFlashMessage({
+            message: expMsg,
+            type: 'warning',
+            visible: true,
+          })
+          if (tokenType === TokenType.Auth) {
+            setTimeout(() => {
+              logout()
+              setFlashMessage(defaultFlashMessage)
+              navigate!('/')
+            }, 3000)
+          }
+        }
+      } else if (nestedError.name === 'JsonWebTokenError') {
+        console.error('[Nested] JsonWebTokenError: Token malformed:', nestedError)
+        if (setFlashMessage) {
+          const malMsg = tokenType === TokenType.Auth ? 'Session verification failed. Try logging out and in again.' : 'Email verification token is malformed. Make sure the verification link is not broken, or re-send link.'
+          setFlashMessage({
+            message: malMsg,
+            type: 'warning',
+            visible: true,
+          })
+        }
+      } else {
+        console.error('Unknown token error:', nestedError)
+        if (setFlashMessage) {
+          const unknownMsg = tokenType === TokenType.Auth ? 'Session unexpectedly ended. Try logging out and in again.' : 'Something went wrong during the email verification. Try re-sending the link.'
+          setFlashMessage({
+            message: unknownMsg,
+            type: 'warning',
+            visible: true,
+          })
+        }
+      }
+    } else {
+      console.log('httpError.name:', httpError.name)
+      console.log('httpError.message:', httpError.message)
+      // console.log('httpError.stack:', httpError.stack)
+      // console.error(`Http Error: ${httpError}`)
+      // if (!setFlashMessage) console.error('error:', httpError)
+      if (setFlashMessage) {
+        const otherMsg = tokenType === TokenType.Auth ? 'Error trying to authorize user' : 'Error trying to verify email'
+        console.error(`${otherMsg}:`, httpError)
+        setFlashMessage({
+          message: `${otherMsg}: ${httpError}`,
+          type: 'error',
+          visible: true,
+        })
+      }
+    }
+  } else {
+    httpError = error as typeof error
+    if (!setFlashMessage) {
+      console.log('httpError.name:', httpError.name)
+      console.log('httpError.message:', httpError.message)
+      // console.log('httpError.stack:', httpError.stack)
+      console.error('httpError:', httpError)
+    }
+    if (setFlashMessage) {
+      const otherMsg = tokenType === TokenType.Auth ? 'Error trying to authorize user' : 'Error trying to verify email'
+      console.error(`${otherMsg}:`, httpError)
+      setFlashMessage({
+        message: `${otherMsg}: ${httpError}`,
+        type: 'error',
+        visible: true,
+      })
+    }
+  }
+}
+
 
 // console.log with console.error styling
 export const consoleError = (errorObj: any): void => {
