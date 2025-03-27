0bdb9a6566e7eadc5d70da7e8e43f38c327660b5
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-27 22:24:55 +0200

    Fixed styling

--- src/components/shared/CustomButton.tsx
+++ src/components/shared/CustomButton.tsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 import type { FC, JSX } from 'react'
 import type { ICustomButton } from '../../types/form.types'
 
-const CustomButton: FC<ICustomButton> = ({ onClick, to, type, children, className, size = 'large', ...props }): JSX.Element => {
+const CustomButton: FC<ICustomButton> = ({ onClick, to, type, children, className, size = 'medium', ...props }): JSX.Element => {
 
   // console.log('css classes from start:', className)
 
-  let classNamesButton = `inline-block bg-button-bg text-button-text border-0 rounded-sm cursor-pointer transition-colors duration-300`
+  let classNamesButton = `inline-block outline-0 bg-button-bg text-button-text border-0 rounded-sm cursor-pointer transition-colors duration-300`
   switch (size) {
     case 'small':
       classNamesButton = `${classNamesButton} leading-[1.2] p-1 px-2 text-xs`
@@ -20,18 +20,19 @@ const CustomButton: FC<ICustomButton> = ({ onClick, to, type, children, classNam
     default:
   }
 
-  // class="pointer-events-none select-none text-button-text leading-[1.2] hover:-translate-y-1"
+  // class="pointer-events-none select-none text-button-text"
 
   let classNamesLink = `${classNamesButton} hover: no-underline visited: text-button-text`
   to ? classNamesLink = className ? `${classNamesLink} ${className}` : classNamesLink :
        classNamesButton = className ? `${classNamesButton} ${className}` : classNamesButton
+  // TODO: pointer-events-none or select-none below?
   return to ? (
     <Link className={classNamesLink} to={to} {...props}>
-      <span className="text-inherit pointer-events-none">{children}</span>
+      <span className="text-inherit select-none">{children}</span>
     </Link>
   ) : (
     <button className={classNamesButton} type={type} onClick={onClick} {...props}>
-      <span className="text-inherit pointer-events-none">{children}</span>
+      <span className="text-inherit select-none">{children}</span>
     </button>
   )
 }
