eb83551d52c476f52cb9a45c1a80ea82b5420930
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-27 22:15:08 +0200

    Switched over to handleHttpError, removed console.logs, changed message texts

--- src/components/Verify.tsx
+++ src/components/Verify.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios, { HttpStatusCode, isAxiosError } from 'axios'
-import type { FC, JSX } from 'react'
+import type { FC, JSX, MouseEvent } from 'react'
 import type { AxiosError, AxiosResponse } from 'axios'
 import Layout from './layout/Layout'
-import DelayedLink from './shared/DelayedLink'
-import { LinkType, TokenType } from '../types/general.types'
+import TextLink from './shared/TextLink'
+import { TokenType } from '../types/general.types'
 import type { IGlobal } from '../types/general.types'
-import { consoleError, handleHttpError, login, scrollSmoothlyToTop } from '../utils/functions'
+import { consoleError, getUserEmail, getUserId, handleHttpError, login, scrollSmoothlyToTop } from '../utils/functions'
 import { defaultFlashMessage } from '../utils/defaults'
 
 const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessage, setFlashMessage, wrapperRef }): JSX.Element => {
@@ -28,16 +28,12 @@ const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessag
           })
           const response: AxiosResponse = await axios.get(`/verify?token=${params.verifyToken}`)
           if (response.status === HttpStatusCode.Ok && response.data) {
-            const { userId, authToken, issued, expires } = response.data
-            // console.log('userId:', userId)
-            // console.log('authToken:', authToken)
-            // console.log('issued:', issued)
-            // console.log('expires:', expires)
-            if (!userId || !authToken || !issued || !expires) throw new Error("Missing Response data content")
-            tokenData = JSON.stringify({ userId, authToken, issued, expires })
+            const { userId, email, authToken, issued, expires } = response.data
+            if (!userId || !email || !authToken || !issued || !expires) throw new Error("Missing Response data content")
+            tokenData = JSON.stringify({ userId, email, authToken, issued, expires })
           }
         } catch (error) {
-          handleHttpError(httpError, error, setFlashMessage, defaultFlashMessage, TokenType.Verify, navigate)
+          httpError = handleHttpError(error, setFlashMessage, defaultFlashMessage, TokenType.Verify, navigate)
         } finally {
           setLoading!(false)
         }
@@ -53,43 +49,48 @@ const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessag
             })
           }, 3000)
           setTimeout(() => {
-            // console.log('tokenData:', tokenData)
-            // console.log('JSON.parse(tokenData):', JSON.parse(tokenData))
             login(tokenData)
             navigate('/home')
+            setFlashMessage(defaultFlashMessage)
           }, 6000)
         }
       })()
     }
   }, [])
 
-  const resendVerificationLink = async () => {
-    let error, userId
+  const resendVerificationLink = async (e: MouseEvent<HTMLAnchorElement>): Promise<void> => {
+    e.preventDefault()
+    let httpError, userId, email
     try {
       setLoading!(true)
       scrollSmoothlyToTop()
-      setFlashMessage({
-        message: 'Sending verification email...',
-        type: 'success',
-        visible: true,
-      })
-      const response: AxiosResponse = await axios.post('/sendVerificationLink', { /* email */ })
-      if (response.status === HttpStatusCode.Created && response.data) {
-        userId = response.data.userId
-      }
-    } catch (e) {
-      if (isAxiosError(e)) {
-        error = e as AxiosError
-        consoleError(error)
+      setFlashMessage(defaultFlashMessage)
+      setTimeout(() => {
         setFlashMessage({
-          message: 'An error occured trying to register',
+          message: 'Resending verification email...',
+          type: 'success',
+          visible: true,
+        })
+      }, 50)
+      const response: AxiosResponse = await axios.post('/register?reSend', { userId: getUserId(), email: getUserEmail() })
+      if (response.status === HttpStatusCode.Ok && response.data) {
+        userId = response.data.userId
+        email = response.data.email
+      }
+    } catch (error) {
+      // httpError = handleHttpError(error, setFlashMessage, defaultFlashMessage, TokenType.Verify, navigate)
+      if (isAxiosError(error)) {
+        httpError = error as AxiosError
+        consoleError(httpError)
+        setFlashMessage({
+          message: 'An error occured trying to resend verification email',
           type: 'error',
           visible: true,
         })
       } else {
-        console.error("Error trying to register:\n", error)
+        console.error("Error trying to resend verification email:\n", httpError)
         setFlashMessage({
-          message: `Error trying to register:<br />${error}`,
+          message: `Error trying to resend verification email:<br />${httpError}`,
           type: 'error',
           visible: true,
         })
@@ -97,11 +98,11 @@ const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessag
     } finally {
       setLoading!(false)
     }
-    if (!error && userId) {
+    if (!httpError && userId && email) {
       setFlashMessage(defaultFlashMessage)
       setTimeout(() => {
         setFlashMessage({
-          message: 'Registration successful! Please check your inbox for an email verification link.',
+          message: 'Email sent successfully! Please check your inbox again for a new email verification link.',
           type: 'success',
           visible: true,
         })
@@ -111,11 +112,9 @@ const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessag
 
   return (
     <Layout loading={loading} theme={theme} setTheme={setTheme} flashMessage={flashMessage} setFlashMessage={setFlashMessage} wrapperRef={wrapperRef}>
+      <h3 className="text-2xl font-bold mb-4">Email Verification</h3>
       <div>
-        <h3 className="text-2xl font-bold mb-4">Email Verification</h3>
-        <div>
-          <p className="mt-4"><DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} onClick={resendVerificationLink} className="ml-1">Re-send verification link</DelayedLink></p>
-        </div>
+        <p className="mt-4"><TextLink to={location.href} onClick={resendVerificationLink} className="ml-1">Re-send verification link</TextLink></p>
       </div>
     </Layout>
   )
