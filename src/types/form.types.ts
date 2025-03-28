85c43ee48455080b42a0d0d96d3d30c743ef406e
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-28 23:23:07 +0200

    Removed type from ICustomButton and added wrapperRef to ILogout

--- src/types/form.types.ts
+++ src/types/form.types.ts
@@ -1,4 +1,4 @@
-import type { MouseEventHandler, ChangeEventHandler, ReactNode } from 'react'
+import type { MouseEventHandler, ChangeEventHandler, ReactNode, RefObject } from 'react'
 
 export interface IAuthForm {
   formType: 'login' | 'register'
@@ -8,7 +8,6 @@ export interface IAuthForm {
 export interface ICustomButton {
   // button: boolean
   className?: string
-  type?: 'button' | 'submit' | 'reset'
   onClick?: MouseEventHandler<HTMLButtonElement>
   size?: 'small' | 'medium' | 'large'
   to?: string
@@ -17,6 +16,7 @@ export interface ICustomButton {
 }
 
 export interface ILogout {
+  wrapperRef: RefObject<HTMLDivElement | null>
   className?: string
   size?: 'small' | 'medium' | 'large'
   [key: string]: any
