30f46406bb7d86a982efbfe843eb2decd4c173fe
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-30 14:44:07 +0300

    Optimized and removed superfluous code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,15 +7,13 @@ import type { IGlobal } from '../types/general.types'
 const Home: FC<IGlobal> = ({ loading, theme, setTheme, flashMessage, setFlashMessage, wrapperRef }): JSX.Element => {
   return (
     <Layout loading={loading} theme={theme} setTheme={setTheme} flashMessage={flashMessage} setFlashMessage={setFlashMessage} wrapperRef={wrapperRef}>
-      <div>
-        <h1 className="text-2xl font-bold mb-4">Welcome to Your Home Page!</h1>
-        <p className="mb-2">
-          Here you can manage your
-          <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="ml-1" to="/articles">Articles</DelayedLink> and your
-          <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="ml-1" to="/profile">Profile</DelayedLink>.
-        </p>
-        <p className="mb-1">Scribble away, and save all your thoughts and ideas in one place!</p>
+      <h3 className="text-2xl font-bold mb-4">Welcome to Your Home Page!</h3>
+      <div className="mb-2">
+        Here you can manage your
+        <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="ml-1" to="/articles">Articles</DelayedLink> and your
+        <DelayedLink wrapperRef={wrapperRef} linkType={LinkType.Text} className="ml-1" to="/profile">Profile</DelayedLink>.
       </div>
+      <div className="mb-1">Scribble away, and save all your thoughts and ideas in one place!</div>
     </Layout>
   )
 }
