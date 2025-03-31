5b7f0dc4adffbfea2fc683865cb3fee1e83cd1fb
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-31 14:49:47 +0300

    Removed unused code

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,15 +1,8 @@
-// import { useEffect, useState } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import type { FC, JSX } from 'react'
 import { isAuthenticated } from '../utils/functions'
-// import { defaultRequestConfig } from '../utils/defaults'
 
 const ProtectedRoute: FC = (): JSX.Element => {
-  // const [authenticated, setAuthenticated] = useState<boolean>(false)
-  // useEffect(() => {
-  //   isAuthenticated(setAuthenticated, defaultRequestConfig)
-  // }, [])
-
   if (!isAuthenticated()) {
     return <Navigate to="/" />
   }
