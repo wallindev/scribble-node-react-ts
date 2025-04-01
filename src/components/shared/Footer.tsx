f75ad71adb798b2535d2d6c5e2c5e8498f3ff717
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-01 21:10:45 +0300

    Removed outline and added a slight darkening on hover on copyright email link

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: FC<IFooter> = ({ theme, setTheme }): JSX.Element => {
   return (
     <div className="my-0 sm:mt-4 mx-auto p-2 pb-0 sm:p-4 sm:pb-0 sm:border-t border-main-content-bg">
       <div className="flex justify-between items-center text-xs">
-        <div>&copy; {(new Date).getUTCFullYear()} <TextLink className="text-text visited:text-text hover:text-text" to="mailto: wallindev@gmail.com">wallindev</TextLink></div>
+        <div>&copy; {(new Date).getUTCFullYear()} <TextLink className="outline-0 text-text visited:text-text hover:text-text-hover focus-visible:text-text-hover" to="mailto: wallindev@gmail.com">wallindev</TextLink></div>
         <div><ThemeSelector theme={theme} setTheme={setTheme} /></div>
       </div>
     </div>
