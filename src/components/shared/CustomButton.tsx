ab835f5e630b114df6ffee8c45b2507fed6528ca
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-01 20:54:21 +0300

    Added hover and focus-visible effects on CustomButton, removed old comments

--- src/components/shared/CustomButton.tsx
+++ src/components/shared/CustomButton.tsx
@@ -2,11 +2,8 @@ import { Link } from 'react-router-dom'
 import type { FC, JSX } from 'react'
 import type { ICustomButton } from '../../types/form.types'
 
-const CustomButton: FC<ICustomButton> = ({ onClick, to, type, children, className, size = 'medium', ...props }): JSX.Element => {
-
-  // console.log('css classes from start:', className)
-
-  let classNamesButton = `inline-block outline-0 bg-button-bg text-button-text border-0 rounded-sm cursor-pointer transition-colors duration-300`
+const CustomButton: FC<ICustomButton> = ({ onClick, to, type = 'button', children, className, size = 'medium', ...props }): JSX.Element => {
+  let classNamesButton = `inline-block outline-0 bg-button-bg text-button-text hover:bg-button-bg-hover focus-visible:bg-button-bg-hover border-0 rounded-sm cursor-pointer transition-colors duration-300`
   switch (size) {
     case 'small':
       classNamesButton = `${classNamesButton} leading-[1.2] p-1 px-2 text-xs`
@@ -19,13 +16,10 @@ const CustomButton: FC<ICustomButton> = ({ onClick, to, type, children, classNam
       break;
     default:
   }
-
-  // class="pointer-events-none select-none text-button-text"
-
   let classNamesLink = `${classNamesButton} hover: no-underline visited: text-button-text`
+
   to ? classNamesLink = className ? `${classNamesLink} ${className}` : classNamesLink :
        classNamesButton = className ? `${classNamesButton} ${className}` : classNamesButton
-  // TODO: pointer-events-none or select-none below?
   return to ? (
     <Link className={classNamesLink} to={to} {...props}>
       <span className="text-inherit select-none">{children}</span>
