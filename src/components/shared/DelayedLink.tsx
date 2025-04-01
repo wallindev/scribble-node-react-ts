2fa9b74a1ddaf5e4103d17acbfdb3047dcc2e661
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-01 21:00:15 +0300

    Cleaned up and removed old unused code, added functionality in handleNavigate

--- src/components/shared/DelayedLink.tsx
+++ src/components/shared/DelayedLink.tsx
@@ -1,68 +1,62 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import classNames from 'classnames'
 import type { FC, JSX, MouseEvent } from 'react'
 import { LinkType } from '../../types/general.types'
 import CustomButton from './CustomButton'
 import TextLink from './TextLink'
-import { FADE_IN_TIME, FADE_OUT_TIME } from '../../utils/constants'
-import { avoidStupidTailwindDuplicationAtLeastInSourceCode } from '../../utils/functions'
+import { FADE_OUT_TIME } from '../../utils/constants'
 import type { IDelayedLink } from '../../types/general.types'
 
-const DelayedLink: FC<IDelayedLink> = ({ wrapperRef, linkType = LinkType.Nav, delay = FADE_OUT_TIME, onClick, to, buttonType, children, className, size = 'large', style, ...props }): JSX.Element => {
-  const [navLinkIsActive, setNavLinkIsActive] = useState<boolean>(false)
+const DelayedLink: FC<IDelayedLink> = ({ wrapperRef, linkType = LinkType.Nav, delay = FADE_OUT_TIME, onClick, to, buttonType = 'button', children, className, size = 'large', style, ...props }): JSX.Element => {
   const navigate = useNavigate()
   const navLinkRef = useRef<HTMLAnchorElement>(null)
 
-  // console.log('css classes from start:', className)
+  let cssClasses: string = ''
+  if (className) cssClasses = classNames(className, cssClasses)
+  cssClasses = classNames('outline-0', cssClasses)
 
   useEffect(() => {
-    if (navLinkRef.current && linkType === LinkType.Nav) {
+    if (linkType === LinkType.Nav && navLinkRef.current) {
       const curNavLink = navLinkRef.current as HTMLAnchorElement
-      const curNavLinkName: string = curNavLink.innerHTML
-      // console.log('current navlink:', curNavLinkName)
-      // console.log('current navlink\'s css classes:', curNavLink.classList)
+      curNavLink.classList.add('hover:text-text-hover', 'focus-visible:text-text-hover', 'transition-colors', 'duration-300')
 
-      if (curNavLink.classList.contains('active')) {
-        console.log(`'${curNavLinkName}' is active! and shouldn't be clickable =)`)
-        // setNavLinkIsActive(true)
-      }
+      if (curNavLink.classList.contains('active')) curNavLink.classList.add('font-bold', 'pointer-events-none', 'select-none')
     }
   }, [])
 
-  const handleNavigate = (e: MouseEvent<HTMLAnchorElement>): void => {
+  const handleNavigate = (e: MouseEvent<HTMLButtonElement | HTMLAnchorElement>): void => {
     e.preventDefault()
-    // If NavLink is active, do nothing
-    if (navLinkIsActive) return
 
-    // Initiate fade-out effect on wrapper div
-    const divWrapper = wrapperRef.current as HTMLDivElement
-    divWrapper.classList?.replace(`duration-${FADE_IN_TIME}`, `duration-${FADE_OUT_TIME}`)
-    divWrapper.classList?.replace('opacity-100', 'opacity-0')
+    // NavLink
+    if (linkType === LinkType.Nav) {
+      const curNavLink = navLinkRef.current as HTMLAnchorElement
+      if (curNavLink.classList.contains('active')) return
+    }
+
+    if (onClick) {
+      // console.log('DelayedLink, onClick:', onClick)
+      onClick(e as MouseEvent<HTMLButtonElement>)
+    }
+
+    setTimeout(() => {
+      const divWrapper = wrapperRef.current as HTMLDivElement
+      divWrapper.classList.replace('opacity-100', 'opacity-0')
+    }, 100)
 
     // Navigate after the fade-out is finished
     setTimeout(() => {
+      // console.log(`Navigating to '${to}'...`)
       navigate(to)
     }, delay)
   }
 
-  // class="focus-visible:text-text-hover"
-  // {() => avoidStupidTailwindDuplicationAtLeastInSourceCode(['active', 'hover', 'focus', 'focus-visible'], ['bg-button-bg-hover', 'text-text-hover'])}
-  // {() => avoidStupidTailwindDuplicationAtLeastInSourceCode(['active', 'hover', 'focus', 'focus-visible'], ['bg-button-bg-hover', 'text-text-hover'])}
-  // {() => avoidStupidTailwindDuplicationAtLeastInSourceCode(['active', 'hover', 'focus', 'focus-visible'], ['bg-button-bg-hover', 'text-text-hover'])}
-
-  // const tailwindClasses = 'hover:bg-button-bg-hover hover:text-text-hover'
-  // const tailwindClassesFunc = () => 'hover:bg-button-bg-hover hover:text-text-hover focus-visible:bg-button-bg-hover focus-visible:text-text-hover'
-  // cssClassesString: hover:bg-button-bg-hover hover:text-text-hover focus-visible:bg-button-bg-hover focus-visible:text-text-hover
-  // cssClassesString: hover:bg-button-bg-hover hover:text-text-hover focus-visible:bg-button-bg-hover focus-visible:text-text-hover
-  // cssClassesString: active:bg-button-bg-hover active:text-text-hover hover:bg-button-bg-hover hover:text-text-hover focus-visible:bg-button-bg-hover focus-visible:text-text-hover
-  // cssClassesString: active:bg-button-bg-hover active:text-text-hover hover:bg-button-bg-hover hover:text-text-hover focus:bg-button-bg-hover focus:text-text-hover focus-visible:bg-button-bg-hover focus-visible:text-text-hover
   if (linkType === LinkType.Text) {
-    return <TextLink className={classNames('outline-0', className)} to={to} {...props}>{children}</TextLink>
+    return <TextLink className={cssClasses} to={to} onClick={handleNavigate} {...props}>{children}</TextLink>
   } else if (linkType === LinkType.Button) {
-    return <CustomButton className={classNames('outline-0', className)} to={to} type={buttonType} onClick={onClick}>{children}</CustomButton>
+    return <CustomButton className={cssClasses} onClick={handleNavigate} type={buttonType} size={size} {...props}>{children}</CustomButton>
   } else {
-    return <NavLink ref={navLinkRef} className={classNames('outline-0', className)} to={to} onClick={handleNavigate} {...props}>{children}</NavLink>
+    return <NavLink ref={navLinkRef} className={cssClasses} to={to} end onClick={handleNavigate} {...props}>{children}</NavLink>
   }
 }
 
