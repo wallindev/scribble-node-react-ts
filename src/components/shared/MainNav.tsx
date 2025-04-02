db4e078c6f11367d282af77da507203fee9d0cad
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-02 21:15:45 +0300

    Added sending props to Logout component to be able to implement all delayed/fade functionality when clicking on logout button

--- src/components/shared/MainNav.tsx
+++ src/components/shared/MainNav.tsx
@@ -1,17 +1,11 @@
 import type { FC, JSX } from 'react'
-import { MouseEvent/* , useEffect, useState */ } from 'react'
+import { MouseEvent } from 'react'
 import DelayedLink from './DelayedLink'
 import Logout from './Logout'
 import type { IMainNav } from '../../types/general.types'
 import { isAuthenticated } from '../../utils/functions'
-// import { defaultRequestConfig } from '../../utils/defaults'
 
-const MainNav: FC<IMainNav> = ({ wrapperRef, subNavOpen, setSubNavOpen }): JSX.Element => {
-  // const [authenticated, setAuthenticated] = useState<boolean>(false)
-  // useEffect(() => {
-  //   isAuthenticated(setAuthenticated, defaultRequestConfig)
-  // }, [])
-
+const MainNav: FC<IMainNav> = ({ wrapperRef,  flashMessage, setFlashMessage, subNavOpen, setSubNavOpen }): JSX.Element => {
   const toggleMenu = (e: MouseEvent<HTMLDivElement>): void => {
     setSubNavOpen!(!subNavOpen)
     e.stopPropagation()
@@ -19,7 +13,7 @@ const MainNav: FC<IMainNav> = ({ wrapperRef, subNavOpen, setSubNavOpen }): JSX.E
 
   const burgerLinesUtilClasses = 'w-6 h-0.5 bg-white mx-0 my-1'
   const linkUtilClasses = 'block border border-t-0 border-main-content-bg px-6 py-4 sm:border-0 sm:inline-block sm:px-1 sm:py-2'
-  // class="border border-t-0 border-main-content-bg"
+
   return (
     <nav className="mb-2 sm:mb-4 p-4 sm:border-b border-main-content-bg">
       <div className="flex justify-between items-center">
@@ -36,13 +30,13 @@ const MainNav: FC<IMainNav> = ({ wrapperRef, subNavOpen, setSubNavOpen }): JSX.E
             <li><DelayedLink wrapperRef={wrapperRef} to="/home" className={linkUtilClasses.replace('border-t-0', '')} title="To Home My Page">Home</DelayedLink></li>
             <li><DelayedLink wrapperRef={wrapperRef} to="/articles" className={linkUtilClasses} title="To My Articles Page">My Articles</DelayedLink></li>
             <li><DelayedLink wrapperRef={wrapperRef} to="/profile" className={linkUtilClasses} title="To My Profile Page">My Profile</DelayedLink></li>
-            <li className={`${linkUtilClasses} flex flex-col items-end`}><Logout wrapperRef={wrapperRef} size="medium" title="Logout of Scribble!" /></li>
+            <li className={`${linkUtilClasses} flex flex-col items-end`}><Logout wrapperRef={wrapperRef} flashMessage={flashMessage} setFlashMessage={setFlashMessage} size="medium" title="Logout of Scribble!" /></li>
           </ul>
           <ul className="hidden sm:flex list-none">
             <li><DelayedLink wrapperRef={wrapperRef} to="/home" className={linkUtilClasses} title="To Home My Page">Home</DelayedLink></li>
             <li><DelayedLink wrapperRef={wrapperRef} to="/articles" className={linkUtilClasses} title="To My Articles Page">My Articles</DelayedLink></li>
             <li><DelayedLink wrapperRef={wrapperRef} to="/profile" className={linkUtilClasses} title="To My Profile Page">My Profile</DelayedLink></li>
-            <li className="flex items-center"><Logout className="ml-2" wrapperRef={wrapperRef} size="small" title="Logout of Scribble!" /></li>
+            <li className="flex items-center"><Logout wrapperRef={wrapperRef} flashMessage={flashMessage} setFlashMessage={setFlashMessage} className="ml-2" size="small" title="Logout of Scribble!" /></li>
           </ul>
         </div>}
       </div>
