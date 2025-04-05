05a5930d8a622bd462e76b7b4dd5ff90d7bb3e5e
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-05 07:53:07 +0200

    Added hideFlashMessage, STANDARD_DELAY, fixed order of reponse return values, added fadeOutAndNavigate function

--- src/components/Verify.tsx
+++ src/components/Verify.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import axios, { HttpStatusCode, isAxiosError } from 'axios'
+import axios, { HttpStatusCode } from 'axios'
 import type { FC, JSX, MouseEvent, RefObject } from 'react'
-import type { AxiosError, AxiosResponse } from 'axios'
+import type { AxiosResponse } from 'axios'
 import Layout from './layout/Layout'
 import TextLink from './shared/TextLink'
 import { TokenType } from '../types/general.types'
-import { consoleError, getUserEmail, getUserId, handleHttpError, login } from '../utils/functions'
+import { fadeOutAndNavigate, getUserEmail, getUserId, handleHttpError, hideFlashMessage, login } from '../utils/functions'
+import { STANDARD_DELAY } from '../utils/constants'
 import type { IGlobal } from '../types/general.types'
 
 const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessage, setFlashMessage, wrapperRef }): JSX.Element => {
@@ -26,9 +27,9 @@ const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessag
           })
           const response: AxiosResponse = await axios.get(`/verify?token=${params.verifyToken}`)
           if (response.status === HttpStatusCode.Ok && response.data) {
-            const { userId, email, authToken, issued, expires } = response.data
-            if (!userId || !email || !authToken || !issued || !expires) throw new Error("Missing Response data content")
-            tokenData = JSON.stringify({ userId, email, authToken, issued, expires })
+            const { userId, email, issued, expires, authToken } = response.data
+            if (!userId || !email || !issued || !expires || !authToken) throw new Error("Missing Response data content")
+            tokenData = JSON.stringify({ userId, email, issued, expires, authToken })
           }
         } catch (error) {
           httpError = handleHttpError(error, wrapperRef as RefObject<HTMLDivElement>, flashMessage, setFlashMessage, TokenType.Verify, navigate)
@@ -37,20 +38,19 @@ const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessag
         }
         if (!httpError && tokenData) {
           setTimeout(() => {
-            setFlashMessage({ ...flashMessage, visible: false })
-          }, 2900)
+            hideFlashMessage(flashMessage, setFlashMessage)
+          }, 2000)
           setTimeout(() => {
             setFlashMessage({
               message: 'Email verification successful! Logging in...',
               type: 'success',
               visible: true,
             })
-          }, 3000)
+          }, STANDARD_DELAY)
+          fadeOutAndNavigate(wrapperRef as RefObject<HTMLDivElement>, '/home', navigate, STANDARD_DELAY * 2, flashMessage, setFlashMessage)
           setTimeout(() => {
             login(tokenData)
-            navigate('/home')
-            setFlashMessage({ ...flashMessage, visible: false })
-          }, 6000)
+          }, STANDARD_DELAY * 2 - 500)
         }
       })()
     }
@@ -61,49 +61,49 @@ const Verify: FC<IGlobal> = ({ loading, setLoading, theme, setTheme, flashMessag
     let httpError, userId, email
     try {
       setLoading!(true)
-      setFlashMessage({ ...flashMessage, visible: false })
+      hideFlashMessage(flashMessage, setFlashMessage)
       setTimeout(() => {
         setFlashMessage({
           message: 'Resending verification email...',
           type: 'success',
           visible: true,
         })
-      }, 50)
+      }, 500)
       const response: AxiosResponse = await axios.post('/register?reSend', { userId: getUserId(), email: getUserEmail() })
       if (response.status === HttpStatusCode.Ok && response.data) {
         userId = response.data.userId
         email = response.data.email
       }
     } catch (error) {
-      // httpError = handleHttpError(error, wrapperRef as RefObject<HTMLDivElement>, flashMessage, setFlashMessage, TokenType.Verify, navigate)
-      if (isAxiosError(error)) {
-        httpError = error as AxiosError
-        consoleError(httpError)
-        setFlashMessage({
-          message: 'An error occured trying to resend verification email',
-          type: 'error',
-          visible: true,
-        })
-      } else {
-        console.error("Error trying to resend verification email:\n", httpError)
-        setFlashMessage({
-          message: `Error trying to resend verification email:<br />${httpError}`,
-          type: 'error',
-          visible: true,
-        })
-      }
+      httpError = handleHttpError(error, wrapperRef as RefObject<HTMLDivElement>, flashMessage, setFlashMessage, TokenType.Verify, navigate)
+      // if (isAxiosError(error)) {
+      //   httpError = error as AxiosError
+      //   consoleError(httpError)
+      //   setFlashMessage({
+      //     message: 'An error occured trying to resend verification email',
+      //     type: 'error',
+      //     visible: true,
+      //   })
+      // } else {
+      //   console.error("Error trying to resend verification email:\n", httpError)
+      //   setFlashMessage({
+      //     message: `Error trying to resend verification email:<br />${httpError}`,
+      //     type: 'error',
+      //     visible: true,
+      //   })
+      // }
     } finally {
       setLoading!(false)
     }
     if (!httpError && userId && email) {
-      setFlashMessage({ ...flashMessage, visible: false })
+      hideFlashMessage(flashMessage, setFlashMessage)
       setTimeout(() => {
         setFlashMessage({
           message: 'Email sent successfully! Please check your inbox again for a new email verification link.',
           type: 'success',
           visible: true,
         })
-      }, 50)
+      }, 1000)
     }
   }
 
