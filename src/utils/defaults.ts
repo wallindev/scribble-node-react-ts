2dd2adb7a51a574a83a40d3cfc5b8dfbb78b09e7
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-05 07:57:57 +0200

    Commenting out old defaultRequestConfig object, will be removed soon if not used for something

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,16 +1,18 @@
-import type { AxiosRequestConfig } from 'axios'
+// import type { AxiosRequestConfig } from 'axios'
 import { Color, MessageType } from '../types/general.types'
 import type { TArticle, TColor, TFlashMessage, TUser } from '../types/general.types'
-import { getAuthToken, hasAuthToken } from './functions'
+// import { getAuthToken, hasAuthToken } from './functions'
 
 export const defaultTitleText = '[Title Here]'
 export const defaultContentText = '[Content Here]'
 
-export const defaultRequestConfig: AxiosRequestConfig | any = hasAuthToken() ? {
-  headers: {
-    Authorization: `Bearer ${getAuthToken()}`
-  }
-} : {}
+// This doesn't work because the token is set the first time this file is being imported,
+// instead being dynamically fetched with every API request. Moving this to a function instead.
+// export const defaultRequestConfig: AxiosRequestConfig | any = hasAuthToken() ? {
+//   headers: {
+//     Authorization: `Bearer ${getAuthToken()}`
+//   }
+// } : {}
 
 export const defaultColors: TColor = {
   success: Color.Green,
