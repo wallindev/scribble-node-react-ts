4f98ae824b11eff8880efbbfe19370c7f3beed78
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-06 08:47:31 +0200

    Added new env values to envConfigVars

--- server/config.js
+++ server/config.js
@@ -20,8 +20,11 @@ if (IS_LOCAL) {
 
 export let envConfigVars = {}
 if (IS_LIVE) {
-  envConfigVars['CSS_PATH']    = process.env.CSS_PATH    || '/css/index.css'
-  envConfigVars['DB_PATH']     = process.env.DB_PATH     || './api-data/db.json'
-  envConfigVars['SECRET_AUTH'] = process.env.SECRET_AUTH || 'b029f401e21027eb9ddb4204f6aad6923c192ea2323b2bbb96d031c2c0a18051'
-  envConfigVars['PORT']        = process.env.PORT        || 3000
-}
+  envConfigVars['CSS_PATH']      = process.env.CSS_PATH      || '/css/index.css'
+  envConfigVars['DB_PATH']       = process.env.DB_PATH       || './api-data/db.json'
+  envConfigVars['SECRET_AUTH']   = process.env.SECRET_AUTH   || 'b029f401e21027eb9ddb4204f6aad6923c192ea2323b2bbb96d031c2c0a18051'
+  envConfigVars['SECRET_VERIFY'] = process.env.SECRET_VERIFY || '66f7b2da8b78dfc1ccb27116e44ecdce5b71f69248992d8c59f126938a3fbd17'
+  envConfigVars['EMAIL_USER']    = process.env.EMAIL_USER    || './api-data/db.json'
+  envConfigVars['EMAIL_PASS']    = process.env.EMAIL_PASS    || 'frtatntzqhooosbh'
+  envConfigVars['PORT']          = process.env.PORT          || 3000
+  }
