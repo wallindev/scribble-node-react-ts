d792f3296f00c33bff55270a396be0bf1d5d2d8c
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-06 09:31:38 +0200

    Changed to correct EMAIL_USER value in envConfigVars

--- server/config.js
+++ server/config.js
@@ -24,7 +24,7 @@ if (IS_LIVE) {
   envConfigVars['DB_PATH']       = process.env.DB_PATH       || './api-data/db.json'
   envConfigVars['SECRET_AUTH']   = process.env.SECRET_AUTH   || 'b029f401e21027eb9ddb4204f6aad6923c192ea2323b2bbb96d031c2c0a18051'
   envConfigVars['SECRET_VERIFY'] = process.env.SECRET_VERIFY || '66f7b2da8b78dfc1ccb27116e44ecdce5b71f69248992d8c59f126938a3fbd17'
-  envConfigVars['EMAIL_USER']    = process.env.EMAIL_USER    || './api-data/db.json'
+  envConfigVars['EMAIL_USER']    = process.env.EMAIL_USER    || 'mikael.wallin@yahoo.se'
   envConfigVars['EMAIL_PASS']    = process.env.EMAIL_PASS    || 'frtatntzqhooosbh'
   envConfigVars['PORT']          = process.env.PORT          || 3000
   }
