c52b2302a1b6a8a79fec0b7ee93f29784c860c29
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-14 16:00:20 +0200

    Removed STANDARD_DELAY, added TRANSITION_DURATION and NAVIGATE_DELAY

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const API_PATH = '/api'
 export const FADE_IN_TIME = 1000
 export const FADE_OUT_TIME = 1000
-export const STANDARD_DELAY = 3000
+export const TRANSITION_DURATION = 1000
+export const NAVIGATE_DELAY = 3000
