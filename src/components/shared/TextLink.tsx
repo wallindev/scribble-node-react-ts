b4e887cf62b2081d19825c28c4ec5eee8f80f478
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-17 21:08:48 +0200

    Added LINK_TRANSITION_DURATION and removed hover and focus-visible transition durations

--- src/components/shared/TextLink.tsx
+++ src/components/shared/TextLink.tsx
@@ -2,9 +2,10 @@ import type { FC, JSX } from 'react'
 import { Link } from 'react-router-dom'
 import classNames from 'classnames'
 import type { ITextLink } from '../../types/general.types'
+import { LINK_TRANSITION_DURATION } from '../../utils/constants'
 
 const TextLink: FC<ITextLink> = ({ className, to, children, style, ...props }): JSX.Element => {
-  let classes = 'text-link visited:text-link active:text-link hover:no-underline hover:text-link-hover focus-visible:text-link-hover transition-colors hover:duration-200 focus-visible:duration-200'
+  let classes = `transition-colors duration-${LINK_TRANSITION_DURATION} text-link visited:text-link active:text-link hover:no-underline hover:text-link-hover focus-visible:text-link-hover`
   // class="bg-button-"
   return (
     <Link className={classNames(classes, className)} to={to} style={style} {...props}>
