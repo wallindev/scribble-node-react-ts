4f205ea4841fb7370ca29d837010c2f398a0b751
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-17 21:11:28 +0200

    Changed fade in, fade out, and transition time to 800 millisecs, added LINK_TRANSITION_DURATION for links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const API_PATH = '/api'
-export const FADE_IN_TIME = 1000
-export const FADE_OUT_TIME = 1000
-export const TRANSITION_DURATION = 1000
+export const FADE_IN_TIME = 800
+export const FADE_OUT_TIME = 800
+export const TRANSITION_DURATION = 800
+export const LINK_TRANSITION_DURATION = 300
 export const NAVIGATE_DELAY = 3000
