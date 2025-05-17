87c51661464c4f13001303d83f9847e1cd8d2ca0
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-17 21:05:56 +0200

    Removed ThemeSelector for now, need to combine the shades in the different color themes better before including the component again

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,15 +1,15 @@
 import type { FC, JSX } from 'react'
 import TextLink from './TextLink'
-import ThemeSelector from './ThemeSelector'
+// import ThemeSelector from './ThemeSelector'
 import type { IFooter } from '../../types/general.types'
 import { TRANSITION_DURATION } from '../../utils/constants'
 
-const Footer: FC<IFooter> = ({ theme, setTheme }): JSX.Element => {
+const Footer: FC<IFooter> = (/* { theme, setTheme } */): JSX.Element => {
   return (
     <div className={`transition-colors duration-${TRANSITION_DURATION} my-0 sm:mt-4 mx-auto p-2 pb-0 sm:p-4 sm:pb-0 sm:border-t border-main-content-bg`}>
       <div className="flex justify-between items-center text-xs">
         <div>&copy; {(new Date).getUTCFullYear()} <TextLink className="outline-0 text-text visited:text-text hover:text-text-hover focus-visible:text-text-hover" to="mailto: wallindev@gmail.com">wallindev</TextLink></div>
-        <div><ThemeSelector theme={theme} setTheme={setTheme} /></div>
+        <div>{/* <ThemeSelector theme={theme} setTheme={setTheme} /> */}</div>
       </div>
     </div>
   )
