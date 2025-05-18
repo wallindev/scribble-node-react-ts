8fc987cea292cd4bfc8afbfa80e1360e12914058
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-18 21:34:36 +0200

    Shifted to having the config in an arrow function, sorted all sections alphabetically

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,40 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindCss from '@tailwindcss/vite'
+import path from 'node:path'
 
 // https://vite.dev/config/
 /** @type {import('vite').UserConfig} */
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindCss(),
-  ],
-  // build: {
-  //   outDir: 'dist',
-  // },
-  server: {
-    // hmr: false,
-    // host: 'localhost', // 0.0.0.0 for all interfaces
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Local API during dev
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.join(process.cwd(), 'server'), '')
+  return {
+    base: '/frontend/',
+    build: {
+      outDir: 'frontend',
+      // outDir: '../public/frontend', // For when it's moved to Laravel structure
+      // emptyOutDir: true,
+    },
+    define: {
+      'process.env': env,
+    },
+    plugins: [
+      react(),
+      tailwindCss(),
+    ],
+    server: {
+      // hmr: false,
+      // host: 'localhost', // 0.0.0.0 for all interfaces
+      proxy: {
+        '/api': {
+          target: `http://localhost:${env.PORT}`, // Local API during dev
+          changeOrigin: true,
+        },
+      },
+      port: 5002,
+      // allowedHosts: ['127.0.0.1', '192.168.32.2', 'grunge', 'grungecorp', 'grunge.dev', 'grungecorp.dev'],
+      watch: {
+        ignored: ['**/api-data/*.*', './TODO.txt'],
       },
     },
-    port: 5000,
-    // allowedHosts: ['127.0.0.1', '192.168.32.2', 'grunge', 'grungecorp', 'grunge.dev', 'grungecorp.dev'],
-    watch: {
-      ignored: ['**/api-data/*.*', './TODO.txt'],
-    },
-  },
+  }
 })
